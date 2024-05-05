387a044fb3842ffa7244ae2513ba316ee008867b
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-05-05 14:49:23 -0500

    ELMINÉ ESAS MIERDAS DE EXTENSIONES DE VSC DE GITHUB NMMS QUE ASCO

--- MINE-00-FirstCode/src/App.java
+++ MINE-00-FirstCode/src/App.java
@@ -3,12 +3,9 @@ import java.util.List;
 import java.util.Scanner;
 
 public class App{
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         Scanner scanner=new Scanner(System.in);
         List<Person> l=new ArrayList<Person>();
-
-        
-        System.out.println("WELCOME");
 
     }
 }

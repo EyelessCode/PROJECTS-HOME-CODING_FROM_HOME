2bcf5eb365a0938b796b27ebe46abfa27c544e8a
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-05-05 14:57:30 -0500

    Hemos agregado un menú único que sólo saldrá por primera vez

--- MINE-00-FirstCode/src/App.java
+++ MINE-00-FirstCode/src/App.java
@@ -6,6 +6,23 @@ public class App{
     public static void main(String[] args) throws Exception{
         Scanner scanner=new Scanner(System.in);
         List<Person> l=new ArrayList<Person>();
+        boolean puerta=false;
 
+        do{
+            Person p=new Person(null, null, 0);
+            
+            System.out.println("=".repeat(35));
+            System.out.println("UNA MUESTRA:");
+            System.out.println("=".repeat(35));
+            System.out.println("MENÚ");
+            System.out.println(">>>> 1. Registrarse.");
+            if(scanner.hasNextInt()){
+                int opcion=scanner.nextInt();
+            }else{
+                System.out.println("=".repeat(140));
+                System.out.println("\n|POR FAVOR INGRESE UN NÚMERO...");
+                System.out.println("_".repeat(140));
+            }
+        } while(!puerta);
     }
 }

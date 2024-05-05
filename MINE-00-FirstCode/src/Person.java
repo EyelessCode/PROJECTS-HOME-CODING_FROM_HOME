997405a1a50c7d1cbd9864a40f042779a2bb78d9
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-05-05 14:19:36 -0500

    Moviendo el ignore y haciendo unos avances en persona

--- MINE-00-FirstCode/src/Person.java
+++ MINE-00-FirstCode/src/Person.java
@@ -1,3 +1,37 @@
 public class Person {
+    private String nombre;
+    private String cedula;
+    private int edad;
 
+    public Person(String nombre, String cedula, int edad) {
+        this.nombre = nombre;
+        this.cedula = cedula;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    
 }

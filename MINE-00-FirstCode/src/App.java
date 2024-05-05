784aab9dcecfe86d8963594ef26976c6d81e0382
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-05-05 15:31:27 -0500

    Se agregó en la rama-b un switch para alternar y condiciones en caso incorrecto

--- MINE-00-FirstCode/src/App.java
+++ MINE-00-FirstCode/src/App.java
@@ -16,8 +16,26 @@ public class App{
             System.out.println("=".repeat(35));
             System.out.println("MENÚ");
             System.out.println(">>>> 1. Registrarse.");
+            System.out.println(">>>> 2. Salir");
             if(scanner.hasNextInt()){
                 int opcion=scanner.nextInt();
+                if(opcion>=1&&opcion<=2){
+                    switch (opcion) {
+                        case 1:
+                            System.out.println("HAS SELECCIONADO LA OPCIÓN "+opcion);
+                            System.out.println("---PROCESANDO---");
+                            System.out.print("Nombre: ");
+                            p.setNombre(scanner.next());
+                            System.out.print("Cédula: ");
+                            p.setCedula(scanner.next());
+                            System.out.print("Edad: ");
+                            p.setEdad(scanner.nextInt());
+                            break;
+                        default:
+                            System.out.println("HAS MARCADO UNA OPCIÓN INVÁLIDA...");
+                            break;
+                    }
+                }
             }else{
                 System.out.println("=".repeat(140));
                 System.out.println("\n|POR FAVOR INGRESE UN NÚMERO...");

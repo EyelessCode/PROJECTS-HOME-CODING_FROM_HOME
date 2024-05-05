5469bfbbd8f89e3f6a646cef157a3f7c8744d7f5
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-05-05 14:43:47 -0500

    Agregando más líneas

--- MINE-00-FirstCode/src/App.java
+++ MINE-00-FirstCode/src/App.java
@@ -1,7 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class App{
     public static void main(String[] args) {
-        
+        Scanner scanner=new Scanner(System.in);
+        List<Person> l=new ArrayList<Person>();
 
+        
         System.out.println("WELCOME");
 
     }

faef02e548f0e59ed829dc1976b6cb222ac62856
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-05-05 19:34:24 -0500

    Agregación de unos métodos

--- MINE-01-IngresoPersona/src/App.java
+++ MINE-01-IngresoPersona/src/App.java
@@ -17,11 +17,17 @@ public class App {
                 case 1:
                     agregar();
                     break;
-            
-                default:
+                case 2:
+                    sectorAyuda();
                     break;
+                case 3:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("DÍGITE ENTRE LAS OPCIONES QUE HAY...");
             }
         }
+        scanner.close();
     }
     
     public static void menuDeBienvenida(){
@@ -46,6 +52,29 @@ public class App {
     }
 
     public static void agregar(){
-        
+        Person p=new Person(null, null, 0);
+
+        System.out.print("Cédula: ");
+        p.setNombre(scanner.next());
+        System.out.print("Nombre: ");
+        p.setNombre(scanner.next());
+        System.out.print("Edad: ");
+        while (!scanner.hasNextInt()) {
+            System.out.println("!".repeat(30));
+            System.out.println("INGRESE UN DÍGITO. NO UNA LETRA");
+            System.out.println("!".repeat(30));            
+        }
+        p.setEdad(scanner.nextInt());
+        System.out.println("PERSONA REGISTRADA");
+        l.add(p);
+    }
+
+    public static void sectorAyuda(){
+        System.out.println("GUÍA ESTABLECIDO. PRESTE ATENCIÓN");
+        System.out.println("1er paso.- Al momento de empezar tiene que digitar una de las opciones a la que quiera acceder. No puede digitar un número fuera de las opciones, mucho menos ingresar una letra.");
+        System.out.println("2do paso.- Luego de digitar e ingresar a una opción...");
+        System.out.println("...");
+        scanner.next();
+        menuDeBienvenida();
     }
 }

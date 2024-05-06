14729203ddadbbc4b2c17afc4d92440d75fa0ac8
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-05-05 20:10:46 -0500

    Desde la app de Github
    No sé we

--- MINE-01-IngresoPersona/src/Main.java
+++ MINE-01-IngresoPersona/src/Main.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class App {
+public class Main {
     public static Scanner scanner=new Scanner(System.in);
     public static List<Person> l=new ArrayList<Person>();
 
@@ -59,12 +59,19 @@ public class App {
         System.out.print("Nombre: ");
         p.setNombre(scanner.next());
         System.out.print("Edad: ");
-        while (!scanner.hasNextInt()) {
+        if(scanner.hasNextInt()){
+            p.setEdad(scanner.nextInt());
+        }else{
             System.out.println("!".repeat(30));
             System.out.println("INGRESE UN DÍGITO. NO UNA LETRA");
-            System.out.println("!".repeat(30));            
+            System.out.println("!".repeat(30));
         }
-        p.setEdad(scanner.nextInt());
+        // while (!scanner.hasNextInt()) {
+        //     System.out.println("!".repeat(30));
+        //     System.out.println("INGRESE UN DÍGITO. NO UNA LETRA");
+        //     System.out.println("!".repeat(30));            
+        // }
+        // p.setEdad(scanner.nextInt());
         System.out.println("PERSONA REGISTRADA");
         l.add(p);
     }
@@ -74,7 +81,7 @@ public class App {
         System.out.println("1er paso.- Al momento de empezar tiene que digitar una de las opciones a la que quiera acceder. No puede digitar un número fuera de las opciones, mucho menos ingresar una letra.");
         System.out.println("2do paso.- Luego de digitar e ingresar a una opción...");
         System.out.println("...");
-        scanner.next();
+        // scanner.next();
         menuDeBienvenida();
     }
 }

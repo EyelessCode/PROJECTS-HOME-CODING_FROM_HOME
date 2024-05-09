e18ad0a30f5c7ae04f4ac0a49d182073c27500b6
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-05-08 21:08:30 -0500

    Se agregó el código de arranque en el MAIN.
    Se agregó la extensión de Application, para que el código abra el programa.

--- MINE-02-FX-SumaNumeros/src/controlador/Main.java
+++ MINE-02-FX-SumaNumeros/src/controlador/Main.java
@@ -1,5 +1,21 @@
 package controlador;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+    @Override
+    public void start(Stage arg0) throws Exception {
+        Parent root=FXMLLoader.load(getClass().getResource("suma.fxml"));
+        Scene ventana=new Scene(root);
+
+        arg0.setTitle("SUMA DE NÚMEROS");
+        arg0.setScene(ventana);
+        arg0.show();
+    }
 
 }

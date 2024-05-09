2dab7362d7649b8e4f7fa19440d83af73676a4a3
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-05-08 21:01:38 -0500

    Se creó la clase del controlador
    La clase fundamental para el programa abra.

---
+++ MINE-02-FX-SumaNumeros/src/controlador/SumaNumero.java
@@ -0,0 +1,27 @@
+package controlador;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class SumaNumero {
+
+    @FXML
+    private Button btnSumar;
+
+    @FXML
+    private TextField txtOp1;
+
+    @FXML
+    private TextField txtOp2;
+
+    @FXML
+    private TextField txtResultado;
+
+    @FXML
+    void sumando(ActionEvent event) {
+
+    }
+
+}

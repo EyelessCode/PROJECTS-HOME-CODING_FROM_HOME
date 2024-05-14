0d5ac4bafe21a7e57bc1f02be8648cd529cfa2c4
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-05-14 15:47:04 -0500

    Cambios en el FXML y agregación de código en el controlador
    Ahora se agregó colores, estética dentro del visual, dentro del controlador se agregó los IDs de los botones.

--- AGAIN-02-FX-SumaNumeros/src/controlador/SumaAgain.java
+++ AGAIN-02-FX-SumaNumeros/src/controlador/SumaAgain.java
@@ -1,6 +1,35 @@
 package vista;
 
+import com.gluonhq.charm.glisten.control.Avatar;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class SumaAgain {
+
+    @FXML
+    private Avatar avaFoto;
+
+    @FXML
+    private Button btnResultado;
+
+    @FXML
+    private TextArea txtConsejo;
+
+    @FXML
+    private TextField txtNum1;
+
+    @FXML
+    private TextField txtNum2;
+
+    @FXML
+    private TextField txtresultado;
+
+    @FXML
+    void proceso(ActionEvent event) {
+
+    }
 
 }

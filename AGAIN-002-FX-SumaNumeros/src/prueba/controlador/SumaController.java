9852b129afaae8ae2f1f92c6ea598466f3dd5661
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-05-24 15:01:21 -0500

    Agregación de contenido en el controlador, creación y agregación de contenido en la clase modelo.
    Se agregó el código correspondiente en la clase controlador, en el modelo se agregó los atributos, constructor y métodos necesarios.

--- AGAIN-002-FX-SumaNumeros/src/prueba/controlador/SumaController.java
+++ AGAIN-002-FX-SumaNumeros/src/prueba/controlador/SumaController.java
@@ -2,8 +2,11 @@ package prueba.controlador;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+import prueba.modelo.Numero;
 
 public class SumaController {
 
@@ -21,7 +24,33 @@ public class SumaController {
 
     @FXML
     void realizacionSuma(ActionEvent event) {
+        try{
+            long num1=Integer.parseInt(this.txtNum1.getText());
+            long num2=Integer.parseInt(this.txtNum2.getText());
 
+            Numero n=new Numero(num1, num2);
+
+            long resultado=n.sumacion();
+
+            this.txtResultado.setText(resultado+"");
+
+            // Consola
+            System.out.println("=".repeat(100));
+            System.out.println("Consola:"+"\nResultado: "+resultado);
+            System.out.println("=".repeat(100));
+
+        }catch (NumberFormatException nfe){
+            Alert alerta=new Alert(AlertType.ERROR);
+            alerta.setTitle("ERROR GENERADO");
+            alerta.setHeaderText("FORMATO INCORRECTO");
+            alerta.setContentText("EL FORMATO QUE ESTÁ APLICANDO ES INVÁLIDO. INTÉNTELO DE NUEVO.");
+
+            // Consola
+            System.out.println("=".repeat(100));
+            System.out.println("Consola:\n>>> ERROR GENERADO\n>>> FORMATO INCORRECTO\nEL FORMATO QUE ESTÁ APLICANDO ES INVÁLIDO. INTÉNTELO DE NUEVO.");
+            System.out.println("=".repeat(100));
+
+            alerta.showAndWait();
+        }
     }
-
 }
---
+++ AGAIN-002-FX-SumaNumeros/src/prueba/modelo/Numero.java
@@ -0,0 +1,31 @@
+package prueba.modelo;
+
+public class Numero {
+    private long a;
+    private long b;
+    
+    public Numero(long a, long b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public long getA() {
+        return a;
+    }
+
+    public void setA(long a) {
+        this.a = a;
+    }
+
+    public long getB() {
+        return b;
+    }
+
+    public void setB(long b) {
+        this.b = b;
+    }
+
+    public long sumacion(){
+        return a+b; 
+    }
+}

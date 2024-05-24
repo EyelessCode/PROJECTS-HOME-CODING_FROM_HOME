e2256f09680c7cd244fb6efe98033cfe358b6855
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-05-24 14:40:03 -0500

    Cambios en el FXML y creación del controlador.
    Se agregó el controlador y cambios ligeros visuales en el visual FXML.

---
+++ AGAIN-002-FX-SumaNumeros/src/prueba/controlador/SumaController.java
@@ -0,0 +1,27 @@
+package prueba.vista;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class SumaController {
+
+    @FXML
+    private Button btnSumar;
+
+    @FXML
+    private TextField txtNum1;
+
+    @FXML
+    private TextField txtNum2;
+
+    @FXML
+    private TextField txtResultado;
+
+    @FXML
+    void realizacionSuma(ActionEvent event) {
+
+    }
+
+}

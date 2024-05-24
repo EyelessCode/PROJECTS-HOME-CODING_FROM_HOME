2d90dbe4014b7e77aa154b556bf5522b9014a80c
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-05-24 00:13:11 -0500

    Cambio de configuración del proyecto "002" y aplicación de nuevo directorio de librerías del proyecto "02".
    Cambio de configuración del proyecto "002" y aplicación de nuevo directorio de librerías del proyecto "02", todos de la categoría "Again".

--- AGAIN-002-FX-SumaNumeros/src/prueba/vista/App.java
+++ AGAIN-002-FX-SumaNumeros/src/prueba/vista/App.java
@@ -1,14 +1,23 @@
 package prueba.vista;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class App extends Application{
     public static void main(String[] args) throws Exception {
-
+        System.out.println("Hello, World!");
     }
 
     @Override
     public void start(Stage arg0) throws Exception {
+        Parent root=FXMLLoader.load(getClass().getResource("sumaFXML.fxml"));
+        Scene ventana=new Scene(root);
+
+        arg0.setTitle("SUMA #3");
+        arg0.setScene(ventana);
+        arg0.show();
     }
 }

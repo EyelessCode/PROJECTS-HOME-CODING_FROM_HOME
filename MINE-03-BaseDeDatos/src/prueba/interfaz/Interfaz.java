8ff683081ef539e2bd9cd3cfca52efb46d63ae8c
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-06-23 19:21:38 -0500

    Actualización en la clase Interfaz.
    Actualización en la clase Interfaz; se añadió un método para conectar la base de datos.

--- MINE-03-BaseDeDatos/src/prueba/interfaz/Interfaz.java
+++ MINE-03-BaseDeDatos/src/prueba/interfaz/Interfaz.java
@@ -1,5 +1,42 @@
 package prueba.interfaz;
 
-public class Interfaz {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+import javafx.application.Application;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+public class Interfaz extends Application{
+    private Connection conexion;
+
+    @Override
+    public void start(Stage arg0) throws Exception {
+        VBox vbox=new VBox();
+
+        Label tituloPrincipal=creacionLabel("BIBLIOTECA", 18);
+    }
+
+    private Label creacionLabel(String texto,double fontSize){
+        Label label=new Label(texto);
+        label.setFont(Font.font(fontSize));
+        label.setTextFill(Color.WHITE);
+        return label;
+    }
+
+    private void baseDatos(){
+        String url="jdbc:mysql://localhost:3306/biblioteca";
+        String usuario="cristhian";
+        String contrasenia="cris03022";
+
+        try{
+            conexion=DriverManager.getConnection(url, usuario, contrasenia);
+        }catch(SQLException se){
+            se.printStackTrace();
+        }
+    }
 }

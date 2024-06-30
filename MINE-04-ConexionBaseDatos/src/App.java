7046d441d44b02bf92bf342022366e33b8e20d62
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-06-30 16:34:22 -0500

    Actualización en la clase App.
    Se creó la consulta y se estableció para que sea ejecutada, se agregó un While con '.next' para que haga un recorrido en cada dato.

--- MINE-04-ConexionBaseDatos/src/App.java
+++ MINE-04-ConexionBaseDatos/src/App.java
@@ -1,15 +1,32 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        String url="jdbc:mysql://localhost:3306/biblioteca";
+        String url="jdbc:mysql://127.0.0.1:3306/biblioteca";
         String user="cristhian";
         String password="cris03022";
 
         try{
             //! AQUÍ ESTABLECES LA CONEXIÓN
             Connection conn=DriverManager.getConnection(url, user, password);
+            
+            //! CREAS LA FRASE PARA LA CONSULTA
+            String sql="SELECT * FROM libro";
+
+            //! CREAR LA CONSULTA
+            Statement stm=conn.createStatement();
+
+            //! ESTABLECES LA CONSULTA Y LA EJECUTAS
+            ResultSet rs=stm.executeQuery(sql);
+
+            while (rs.next()) {//! EL '.next' LO QUE HACE ES RECORRER REGISTRO A REGISTRO
+                
+            }
+
+            // MENSAJES EN CONSOLA
             System.out.println("=".repeat(40));
             System.out.println("CONECTANDO...\nCONEXIÓN TERMINADA\n¡'BIBLIOTECA' CONECTADA!");
             System.out.println("=".repeat(40));

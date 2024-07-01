d469557e96f8286f4b2327c2e934c9e221d1a4f4
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-07-01 18:16:50 -0500

    Creación y actualización en el modelo LibrosRepositorio
    Se está creando un método para llamar a los datos de la tabla Libro de la BD.

---
+++ MINE-02-FX-SumaNumeros/AGAIN-04-ConexionBaseDatos/src/repositorio/LibrosRepositorio.java
@@ -0,0 +1,13 @@
+package repositorio;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import modelo.Libros;
+
+public class LibrosRepositorio {
+    public List<Libros> llamadaLibro(){
+        List<Libros> listaLibros=new ArrayList<Libros>();
+        return null;
+    }
+}

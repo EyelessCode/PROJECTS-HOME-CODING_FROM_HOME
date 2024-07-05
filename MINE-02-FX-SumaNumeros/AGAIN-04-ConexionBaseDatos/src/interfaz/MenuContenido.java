6404c445dee1f1132fdaca862ab9022b6790b4b6
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-07-05 18:58:20 -0500

    Creación de la clase MenuContenido.
    Aún se sigue probando cosas, se creó la clase y se agregó un método que servirá de puerta para el menú.

---
+++ MINE-02-FX-SumaNumeros/AGAIN-04-ConexionBaseDatos/src/interfaz/MenuContenido.java
@@ -0,0 +1,17 @@
+package interfaz;
+
+public class MenuContenido {
+    public void contenidoOpciones(){
+        boolean puerta=true;
+
+        while (puerta) {
+            
+        }
+    }
+
+    public void menu(){
+        System.out.println("1. PRESENTAR LA TABLA 'LIBRO'.");
+        System.out.println("2. PRESENTAR EL CAMPO 'TÍTULO' DE LA TABLA 'LIBRO'");
+        System.out.println("3. SALIR.");
+    }
+}

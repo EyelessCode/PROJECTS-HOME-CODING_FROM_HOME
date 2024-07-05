676a2202f88c88d3cddccfd5b6e4777503a75875
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-07-05 18:36:07 -0500

    Actualización ligera en App.
    Se agregó una correcta presentación de los datos en pantalla.

--- MINE-02-FX-SumaNumeros/AGAIN-04-ConexionBaseDatos/src/App.java
+++ MINE-02-FX-SumaNumeros/AGAIN-04-ConexionBaseDatos/src/App.java
@@ -9,13 +9,21 @@ public class App {
 
         List<Libros> metodoLibros=librosRepositorio.libroGeneral();
         System.out.println("=".repeat(35)+"\nHAY UN TOTAL DE: "+metodoLibros.size()+" DE FILAS.\n"+"=".repeat(35));
+        
+        System.out.println("=".repeat(80)+"\nID  "+"TÍTULO\t"+"ISBN\t\t"+"N° PÁGINAS   "+"FECHA DE PUBLICACIÓN   "+"ID Autor   "+"ID CATEGORÍA   "+"ID EDITORIAL\n");
+        for (Libros libros : metodoLibros) {
+            System.out.println(libros.getId()+"   "+libros.getTitulo()+"\t"+libros.getIsbn()+"\t\t"+libros.getNumPaginas()+"   "+libros.getFechaPublicacion()+"   "+libros.getIdAutor()+"   "+libros.getIdCategoria()+"   "+libros.getIdEditorial()+"\n");
+        }
+        System.out.println("=".repeat(80));
 
         // String sql="titulo";
         List<Libros> metodoTitulo=librosRepositorio.libroTitulo(null);
         // System.out.println("=".repeat(35)+"\n"+metodoLibros+"\n"+"=".repeat(35));
 
+        System.out.println("=".repeat(35)+"\nTÍTULO\n");
         for (Libros libros : metodoTitulo) {
-            System.out.println("=".repeat(35)+"\n"+libros.getTitulo()+"\n"+"=".repeat(35));
+            System.out.println(libros.getTitulo()+"\n");
         }
+        System.out.println("=".repeat(35));
     }
 }

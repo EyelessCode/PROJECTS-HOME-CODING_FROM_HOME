35074cf2b10daa2a15b54110d0c97225c4d20acc
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-08-01 14:50:18 -0500

    Actualización en index.
    Se agregó contenido.

---
+++ HTML-00-Prueba/Java_Script/index.js
@@ -0,0 +1,12 @@
+const email="cristhianjosue03@outlook.es";
+email="cristhianguedes@outlook.es"; //No funcionará, ya que está ya definido.
+
+if (true) {
+    var nombre="HOLA MUNDO";
+    let nombre1="HOLA XD";
+    
+}
+
+console.log("A VER SI EJECUTA: "+nombre);
+console.log(email);
+// console.log("INTENTO: "+nombre1); // <-- Es una variable temporal, no funciona en todo sitio.

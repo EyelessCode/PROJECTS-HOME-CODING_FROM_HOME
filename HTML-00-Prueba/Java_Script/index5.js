a19481a1ab4c25c772d9996d89ddd6a692bd22bd
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-08-01 17:31:31 -0500

    Actualización en adelantadoJS5 y index5.
    Se agregó contenido en ambos.

---
+++ HTML-00-Prueba/Java_Script/index5.js
@@ -0,0 +1,21 @@
+var color=prompt("INGRESE UN COLOR: ");
+
+switch (color) {
+    case "rojo":
+        console.log("ES ROJO.");
+        
+        break;
+    case "azul":
+        console.log("ES AZUL.");
+
+        break;
+    case "amarrillo":
+        console.log("ES AMARILLO.");
+        
+        break;
+    default:
+        alert("COLOR INGRESADO NO ES VÁLIDO.")
+        console.log("COLOR INGRESADO NO ES VÁLIDO.");
+        
+        break;
+}

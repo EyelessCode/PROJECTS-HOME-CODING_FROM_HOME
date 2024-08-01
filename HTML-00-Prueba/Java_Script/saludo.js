1e0b2bd3997b506dc2a4a73468ef460d6319672a
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-08-01 14:39:26 -0500

    Actualización en saludo.
    Se agregó más contenido.

--- HTML-00-Prueba/Java_Script/saludo.js
+++ HTML-00-Prueba/Java_Script/saludo.js
@@ -1,6 +1,13 @@
 var nombre="CRISTHIAN";
-var edad;
-var presencia;
-var apellido;
+var edad=21;
+var presencia=true;
+var apellido="GUEDES";
+var estatura=1.67;
 
-alert("HOLA MUNDO CON JAVASCRIPT y "+nombre);
+console.log("NOMBRE: "+nombre+" "+typeof nombre);
+console.log("EDAD: "+edad+" "+typeof edad);
+console.log("ASISTENCIA: "+presencia+" "+typeof presencia);
+console.log("APELLIDO: "+apellido+" "+typeof apellido);
+console.log("ESTATURA: "+estatura+" "+typeof estatura);
+
+// alert("HOLA MUNDO CON JAVASCRIPT y "+nombre);

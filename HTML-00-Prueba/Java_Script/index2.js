2d838c4c3e5e0450e006ed140da37c697a6cc3ef
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-08-01 15:02:22 -0500

    Actualización en index2 y creación de un nuevo archivo HTML.
    Se agregó bloques de código en index.
    Se creó un archivo html.

---
+++ HTML-00-Prueba/Java_Script/index2.js
@@ -0,0 +1,17 @@
+// OPERACIONES ARITMÉTICOS
+var suma=10+15;
+console.log(suma);
+
+var resta=5-1;
+console.log(resta);
+
+var multiplicacion=2*10;
+console.log(multiplicacion);
+
+var division=6/2;
+console.log(division);
+
+var restante=60%2;
+console.log(restante);
+
+// OPERACIONES DE COMPARACIÓN

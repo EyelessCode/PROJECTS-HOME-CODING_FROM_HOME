887541bae63a1f394c1e5f2c2d152e078d901374
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-08-01 16:40:59 -0500

    Actualización en adelantadoJS3_EJ y index3.
    Se agregó contenido en ambos archivos.

---
+++ HTML-00-Prueba/Java_Script/index3.js
@@ -0,0 +1,8 @@
+var totalComida=2800+1300;
+var totalBebida=900+650;
+var total=totalComida+totalBebida;
+
+alert("TOTAL DE COMIDA Y BEBIDA ES: "+total)
+console.log("Total de comida: "+totalComida);
+console.log("Total de bebida: "+totalBebida);
+console.log("Total de comida y bebida: "+total);

25305fc6a2570db4acd29a83ca74904d81cca72d
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-08-14 16:26:12 -0500

    Cambio de nombre de una clase.
    Se cambió el nombre de la clase a uno más acorde a lo que se va a realizar.

--- Java-MINE-START-BEGINNER/src/beginner/HelloWorld.java
+++ Java-MINE-START-BEGINNER/src/beginner/HelloWorld.java
@@ -1,5 +1,6 @@
 package beginner;
-public class App {
+
+public class HelloWorld {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }

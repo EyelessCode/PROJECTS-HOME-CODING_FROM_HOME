21d9e8c69c29970ae30794de1e3f6872b468e694
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-08-14 16:29:27 -0500

    Actualización en HelloWorld.
    Se agregó contenido.

--- Java-MINE-START-BEGINNING/src/beginner/HelloWorld.java
+++ Java-MINE-START-BEGINNING/src/beginner/HelloWorld.java
@@ -3,5 +3,7 @@ package beginner;
 public class HelloWorld {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        System.out.println("Ya se el print, no te preocupes"+
+        " lo sé, es que estoy 'recordando'");
     }
 }

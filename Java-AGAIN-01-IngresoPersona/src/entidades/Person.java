e09fe1c9689cf8813ce57e820651e50cec404bc8
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-08-20 15:40:35 -0500

    Creación de una Entidad.
    Se creó una entidad para que sea el cuerpo para el sistema.

---
+++ Java-AGAIN-01-IngresoPersona/src/entidades/Person.java
@@ -0,0 +1,144 @@
+package entidades;
+
+public class Person {
+    // private int id;
+    private String name;
+    private String lastName;
+    private char genre;
+    private int age;
+    private String address;
+    private String phone;
+    private String email;
+    private String password;
+    private String role;
+    private String idc;
+    
+    // public Person(int id, String name, String lastName, char genre, int age, String address, String phone, String email,
+    //         String password, String role, String idc) {
+    //     this.name = name;
+    //     this.lastName = lastName;
+    //     this.genre = Character.toUpperCase(genre);
+    //     this.age = age;
+    //     this.address = address;
+    //     this.phone = phone;
+    //     this.email = email;
+    //     this.password = password;
+    //     this.role = role;
+    //     this.idc = idc;
+    // }
+    public Person(String name, String lastName, char genre, int age, String address, String phone, String email,
+            String password, String role, String idc) {
+        this.name = name;
+        this.lastName = lastName;
+        this.genre = Character.toUpperCase(genre);
+        this.age = age;
+        this.address = address;
+        this.phone = phone;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.idc = idc;
+    }
+
+    // public int getId() {
+    //     return id;
+    // }
+
+    // public void setId(int id) {
+    //     this.id = id;
+    // }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public char getGenre() {
+        return genre;
+    }
+
+    public void setGenre(char genre) {
+        this.genre = genre;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getIdc() {
+        return idc;
+    }
+
+    public void setIdc(String idc) {
+        this.idc = idc;
+    }
+
+    // @Override
+    // public String toString() {
+    //     return "Person [id=" + id + ", name=" + name + ", lastName=" + lastName + ", genre=" + genre + ", age=" + age
+    //             + ", address=" + address + ", phone=" + phone + ", email=" + email + ", password=" + password
+    //             + ", role=" + role + ", idc=" + idc + "]";
+    // }
+    @Override
+    public String toString() {
+        return "Person [name=" + name + ", lastName=" + lastName + ", genre=" + genre + ", age=" + age
+                + ", address=" + address + ", phone=" + phone + ", email=" + email + ", password=" + password
+                + ", role=" + role + ", idc=" + idc + "]";
+    }
+
+}

f4ceee63fc8dda8df52658710e191a77adc2dab0
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-09-20 15:04:07 -0500

    Big update in MenuRepository.
    I added methods; these methods are options, the options serve to you can put a right option, if you put a wrong option appear an error and you can put an option again. You can only put a right option if you want to continue.

--- Java-AGAIN-FINAL-IngresoPersona/src/repository/MenuRepository.java
+++ Java-AGAIN-FINAL-IngresoPersona/src/repository/MenuRepository.java
@@ -1,5 +1,183 @@
 package repository;
 
-public class MenuRepository {
+import java.util.Scanner;
 
+import interfaces.MenuInterface;
+
+public class MenuRepository {
+    private MenuInterface mi=new MenuInterface();
+
+    Scanner scanner=new Scanner(System.in);
+
+    public int mainMenuOption(){ //! MAIN MENU OPTION
+        int option=-1;
+
+        while (option<1&&option>3) {
+            System.out.print("\n"+"|"+"-".repeat(5)+">: ");
+
+            while (!scanner.hasNextInt()) {
+                System.out.println("\n"+"=".repeat(50)
+                +"\nINPUT ERROR. TRY TO PUT A VALIDED INPUT...\n"
+                +"=".repeat(50)+"\n");
+                scanner.nextLine();
+            }
+            
+            option=scanner.nextInt();
+            if (option<1||option>3) {
+                System.out.println("\n"+"=".repeat(50)
+                +"\nINVALIDED NUMBER. TRY TO PUT A NUMBER BETWEEN 1 AND 3...\n"
+                +"=".repeat(50)+"\n");
+            }
+
+        }
+        return option;
+    }
+
+    public int inputMenuOption(){ //!INPUT MENU OPTION
+        int option=-1;
+
+        while (option<1&&option>4) {
+            System.out.print("\n"+"|"+"-".repeat(5)+">: ");
+
+            while (!scanner.hasNextInt()) {
+                System.out.println("\n"+"=".repeat(50)
+                +"\nINPUT ERROR. TRY TO PUT A VALIDED INPUT...\n"
+                +"=".repeat(50)+"\n");
+                scanner.nextLine();
+            }
+            
+            option=scanner.nextInt();
+            if (option<1||option>4) {
+                System.out.println("\n"+"=".repeat(50)
+                +"\nINVALIDED NUMBER. TRY TO PUT A NUMBER BETWEEN 1 AND 4...\n"
+                +"=".repeat(50)+"\n");
+            }
+
+        }
+        return option;
+    }
+
+    public int adminMenuOption(){ //! ADMIN MENU OPTION
+        int option=-1;
+
+        while (option<1&&option>4) {
+            System.out.print("\n"+"|"+"-".repeat(5)+">: ");
+
+            while (!scanner.hasNextInt()) {
+                System.out.println("\n"+"=".repeat(50)
+                +"\nINPUT ERROR. TRY TO PUT A VALIDED INPUT...\n"
+                +"=".repeat(50)+"\n");
+                scanner.nextLine();
+            }
+            
+            option=scanner.nextInt();
+            if (option<1||option>4) {
+                System.out.println("\n"+"=".repeat(50)
+                +"\nINVALIDED NUMBER. TRY TO PUT A NUMBER BETWEEN 1 AND 4...\n"
+                +"=".repeat(50)+"\n");
+            }
+
+        }
+        return option;
+    }
+
+    //? ADMIN
+    public int adminChangeMenuOption(){ //! ADMIN CHANGE MENU OPTION
+        int option=-1;
+
+        while (option<1&&option>4) {
+            System.out.print("\n"+"|"+"-".repeat(5)+">: ");
+
+            while (!scanner.hasNextInt()) {
+                System.out.println("\n"+"=".repeat(50)
+                +"\nINPUT ERROR. TRY TO PUT A VALIDED INPUT...\n"
+                +"=".repeat(50)+"\n");
+                scanner.nextLine();
+            }
+            
+            option=scanner.nextInt();
+            if (option<1||option>4) {
+                System.out.println("\n"+"=".repeat(50)
+                +"\nINVALIDED NUMBER. TRY TO PUT A NUMBER BETWEEN 1 AND 4...\n"
+                +"=".repeat(50)+"\n");
+            }
+
+        }
+        return option;
+    }
+
+    //? ADMIN
+    public int adminDeleteMenuOption(){ //! ADMIN DELETE MENU OPTION
+        int option=-1;
+
+        while (option<1&&option>4) {
+            System.out.print("\n"+"|"+"-".repeat(5)+">: ");
+
+            while (!scanner.hasNextInt()) {
+                System.out.println("\n"+"=".repeat(50)
+                +"\nINPUT ERROR. TRY TO PUT A VALIDED INPUT...\n"
+                +"=".repeat(50)+"\n");
+                scanner.nextLine();
+            }
+            
+            option=scanner.nextInt();
+            if (option<1||option>4) {
+                System.out.println("\n"+"=".repeat(50)
+                +"\nINVALIDED NUMBER. TRY TO PUT A NUMBER BETWEEN 1 AND 4...\n"
+                +"=".repeat(50)+"\n");
+            }
+
+        }
+        return option;
+    }
+
+    //? ADMIN
+    public int adminVisualizationMenuOption(){ //! ADMIN VISUALIZATION MENU OPTION
+        int option=-1;
+
+        while (option<1&&option>5) {
+            System.out.print("\n"+"|"+"-".repeat(5)+">: ");
+
+            while (!scanner.hasNextInt()) {
+                System.out.println("\n"+"=".repeat(50)
+                +"\nINPUT ERROR. TRY TO PUT A VALIDED INPUT...\n"
+                +"=".repeat(50)+"\n");
+                scanner.nextLine();
+            }
+            
+            option=scanner.nextInt();
+            if (option<1||option>5) {
+                System.out.println("\n"+"=".repeat(50)
+                +"\nINVALIDED NUMBER. TRY TO PUT A NUMBER BETWEEN 1 AND 5...\n"
+                +"=".repeat(50)+"\n");
+            }
+
+        }
+        return option;
+    }
+
+    //? INPUT
+    public int inputSignInMenuOption(){ //! INPUT SIGN IN MENU OPTION
+        int option=-1;
+
+        while (option!=1) {
+            System.out.print("\n"+"|"+"-".repeat(5)+">: ");
+
+            while (!scanner.hasNextInt()) {
+                System.out.println("\n"+"=".repeat(50)
+                +"\nINPUT ERROR. TRY TO PUT A VALIDED INPUT...\n"
+                +"=".repeat(50)+"\n");
+                scanner.nextLine();
+            }
+            
+            option=scanner.nextInt();
+            if (option==1) {
+                System.out.println("\n"+"=".repeat(50)
+                +"\nINVALIDED NUMBER. TRY TO PUT THE NUMBER 1...\n"
+                +"=".repeat(50)+"\n");
+            }
+
+        }
+        return option;
+    }
 }

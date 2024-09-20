45b45b861267575293c783722cec540a06be7dd0
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-09-19 22:52:35 -0500

    Creating of Person entity.
    Creating of Person entity, i adding its properties

---
+++ Java-AGAIN-FINAL-IngresoPersona/src/entities/Person.java
@@ -0,0 +1,11 @@
+package entities;
+
+public class Person {
+    private String name;
+    private String lastname;
+    private String ic;
+    private char sex;
+    private int age;
+    private String rol;
+    private String password;
+}

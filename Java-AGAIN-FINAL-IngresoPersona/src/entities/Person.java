cf33b764463a0ddfbe1edb2547f6f80438f5444f
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-09-20 14:19:58 -0500

    Update in Person entity.
    Now it has its constructor empty and fill too, and getters and setters.

--- Java-AGAIN-FINAL-IngresoPersona/src/entities/Person.java
+++ Java-AGAIN-FINAL-IngresoPersona/src/entities/Person.java
@@ -8,4 +8,75 @@ public class Person {
     private int age;
     private String rol;
     private String password;
+    
+    public Person() {
+    }
+
+    public Person(String name, String lastname, String ic, char sex, int age, String rol, String password) {
+        this.name = name;
+        this.lastname = lastname;
+        this.ic = ic;
+        this.sex = Character.toUpperCase(sex);
+        this.age = age;
+        this.rol = rol;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getIc() {
+        return ic;
+    }
+
+    public void setIc(String ic) {
+        this.ic = ic;
+    }
+
+    public char getSex() {
+        return sex;
+    }
+
+    public void setSex(char sex) {
+        this.sex = sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    
 }

d45e4e4a669cf5cd77ea4d024e2ee81e781e126d
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-09-20 13:00:35 -0500

    Update in Admin entity.
    I added its properties, i don't need it for now but i will soon.

--- Java-AGAIN-FINAL-IngresoPersona/src/entities/Admin.java
+++ Java-AGAIN-FINAL-IngresoPersona/src/entities/Admin.java
@@ -1,5 +1,10 @@
 package entities;
 
 public class Admin {
-
+    private String username;
+    private String ic;
+    private char sex;
+    private int age;
+    private String rol;
+    private String PASSWORD;
 }

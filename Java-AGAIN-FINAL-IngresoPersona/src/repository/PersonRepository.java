ed99492280c272abbb41b9d4669c76dcd20700f9
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-09-23 18:00:00 -0500

    Update in PersonRepository.
    I added a new method about 'age', it works to validate a right input; just numbers not letters, for now i still working on it.

--- Java-AGAIN-FINAL-IngresoPersona/src/repository/PersonRepository.java
+++ Java-AGAIN-FINAL-IngresoPersona/src/repository/PersonRepository.java
@@ -1,5 +1,35 @@
 package repository;
 
-public class PersonRepository {
+import java.util.Scanner;
 
+import entities.Person;
+
+public class PersonRepository {
+    // private Person pe=new Person();
+    private Scanner scanner=new Scanner(System.in);
+
+    //? AGE METHOD
+    // INPUT A PERSON
+    public int ageInput(Person p){
+        p.setAge(-1); // CENTINEL
+        boolean door=false; // todo: THE DOOR WILL ALLOW THE CONDITIONS WORK
+
+        while (!door) {
+            System.out.print("\nENTER A AGE: ");
+            String input=scanner.nextLine(); // IT WILL WORKS FOR THE CONVERTION
+
+            try {
+                p.setAge(Integer.parseInt(input)); // MAKING THE CONVERTION
+
+                if (p.getAge()<=0) {
+                    System.out.println("PLEASE YOUR AGE CANNOT BE '0' OR A NEGATIVE NUMBER. PUT YOUR REAL AGE...");
+                }else if (p.getAge()>120) {
+                    System.out.println("PLEASE THAT'S AN IMPOSSIBLE AGE, PUT A REAL AGE...");
+                }
+            } catch (NumberFormatException nfe) {
+                System.out.println("THAT'S NOT A NUMBER, PLEASE PUT A RIGHT INPUT...");
+            }
+        }
+        return p.getAge();
+    }
 }

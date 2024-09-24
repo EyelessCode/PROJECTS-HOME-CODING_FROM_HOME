889740e9b052fef4533460fb6e31a4fa0d9921ff
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-09-23 20:50:21 -0500

    Little change in App.
    I removed the basic print...

--- Java-AGAIN-FINAL-IngresoPersona/src/test/App.java
+++ Java-AGAIN-FINAL-IngresoPersona/src/test/App.java
@@ -6,8 +6,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         OptionRepository or=new OptionRepository();
 
-        System.out.println("Hello, World!");
-
         or.switchesMainMenu();        
     }
 }

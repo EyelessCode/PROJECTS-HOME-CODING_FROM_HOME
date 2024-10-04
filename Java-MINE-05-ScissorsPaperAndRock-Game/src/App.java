d4547829453f16cbc916af10d0efe09810ba50f3
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-10-03 20:11:26 -0500

    Little change.
    It doesn't matter... nwo i'll change this...

--- Java-MINE-05-ScissorsPaperAndRock-Game/src/App.java
+++ Java-MINE-05-ScissorsPaperAndRock-Game/src/App.java
@@ -8,45 +8,44 @@ public class App {
         Scanner sn=new Scanner(System.in);
         NumberFor nf=new NumberFor(0, 0);
         Random r=new Random();
-        boolean door=false;
 
-        nf.setA(r.nextInt(1,3));
         nf.setB(r.nextInt(1,3));
 
-        System.out.print("1 = ROCK\n"+"2 = PAPER\n"+"3 = SCISSOR\n"+"→: ");
+        System.out.print("1 = ROCK\n"+"2 = PAPER\n"+"3 = SCISSOR\n"+"-->: ");
         
         while (!sn.hasNextInt()) {
             System.out.println("JUST NUMBERS NOT LETTERS OR SOMETHING ELSE...");
+            System.out.print("1 = ROCK\n"+"2 = PAPER\n"+"3 = SCISSOR\n"+"-->: ");
             sn.nextLine();
         }
-        int input=sn.nextInt();
+        nf.setA(sn.nextInt());
         sn.nextLine();
 
             
-        if (input<=3||input>=1) {
-            if (input==3) { //
+        if (nf.getA()<=3||nf.getA()>=1) {
+            if (nf.getA()==3) { //
                 System.out.println("YOU PICKED 'SCISSOR'");
-                if (nf.getA()==input) {
+                if (nf.getB()==nf.getA()) {
                     System.out.println("MACHINE PULLS 'SCISSOR'"+"\nDRAW!");
-                }else if (nf.getA()==2) {
+                }else if (nf.getB()==2) {
                     System.out.println("MACHINE PULLS 'PAPER'"+"\nYOU WON!");
                 }else{
                     System.out.println("MACHINE PULLS 'ROCK'"+"\nYOU LOSE!");
                 }
-            }else if (input==2) { //
+            }else if (nf.getA()==2) { //
                 System.out.println("YOU PICKED 'PAPER'");
-                if (nf.getA()==input) {
+                if (nf.getB()==nf.getA()) {
                     System.out.println("MACHINE PULLS 'PAPER'"+"\nDRAW!");
-                }else if (nf.getA()==3) {
+                }else if (nf.getB()==3) {
                     System.out.println("MACHINE PULLS 'SCISSOR'"+"\nYOU LOSE!");
                 }else{
                     System.out.println("MACHINE PULLS 'ROCK'"+"\nYOU WON!");
                 }
             }else { //
                 System.out.println("YOU PICKED 'ROCK'");
-                if (nf.getA()==input) {
+                if (nf.getB()==nf.getA()) {
                     System.out.println("MACHINE PULLS 'ROCK'"+"\nDRAW!");
-                }else if (nf.getA()==3) {
+                }else if (nf.getB()==3) {
                     System.out.println("MACHINE PULLS 'SCISSOR'"+"\nYOU WON!");
                 }else {
                     System.out.println("MACHINE PULLS 'PAPER'"+"\nYOU LOSE!");
@@ -57,6 +56,6 @@ public class App {
             System.out.println("INPUT A NUMBER BETWEEN 1 AND 3...");
         }
 
-            
+        sn.close();
     }
 }

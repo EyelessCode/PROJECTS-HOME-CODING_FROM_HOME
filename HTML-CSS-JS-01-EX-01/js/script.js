56054725b79e58f1b6e5deea40e30ab7aa597b15
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-12-01 15:19:25 -0500

    Edit in style and index, update in script.
    In style, we add each buttom as 'cursor: pointer'; i mean, if you pass on the buttom, your cursor will change its form.
    
    In index, i install the script on the page and i add const values in some inputs.
    
    In script, i done with the combobox, buttoms events.

---
+++ HTML-CSS-JS-01-EX-01/js/script.js
@@ -0,0 +1,87 @@
+import { customerList,sitieList } from "./BD.js";
+
+const txtCode=document.getElementById('txtCode');
+const btnSearch=document.getElementById('btnSearch');
+
+const txtIc=document.getElementById('txtIc');
+const txtNames=document.getElementById('txtNames');
+
+const cmbSities=document.getElementById('cmbSities');
+const txtKms=document.getElementById('txtKms');
+const txtCost=document.getElementById('txtCost');
+
+const txtPeople=document.getElementById('txtPeople');
+const txtSubtotal=document.getElementById('txtSubtotal');
+const txtIva=document.getElementById('txtIva');
+const txtTotal=document.getElementById('txtTotal');
+const btnCalculate=document.getElementById('btnCalculate');
+const btnClear=document.getElementById('btnClear');
+
+window.addEventListener('DOMContentLoaded',()=>{
+    sitieList.forEach((place)=>{
+        let option=document.createElement('option');
+        option.value=place.id;
+        option.textContent=place.place;
+        cmbSities.appendChild(option);
+    });
+});
+
+cmbSities.addEventListener('click',()=>{
+    /* Acá sólo se obtiene el index del 'combobox' */
+    let i=cmbSities.selectedIndex;
+
+    /* Acá estamos instalando el index en los valores de las 
+    opciones del 'combobox' (opciones) */
+    let code=cmbSities.options[i].value;
+
+    /* Acá estamos creando una variable que tiene almacenada
+    la lista importada de clientes, lo que hará es hacer una
+    una búsqueda, si es equivalente el index con el id del valor de
+    la propiedad del array; entonces, almacenará en la variable
+    el objeto del array.
+    ! En pocas palabras, agarramos un index, si es igual al id
+    ! del objeto obtienes el objeto. */
+    const select=sitieList.find(place=>place.id==parseInt(code));
+
+    /* Gracias a lo de arriba, podemos establecerlo para establecer
+    valores en los inputs. */
+    txtKms.value=select.travelInKm;
+    txtCost.value=select.cost;
+});
+
+btnSearch.addEventListener('click',()=>{
+    //// let door = false;
+    /* Obtengo el valor del input code. */
+    const codigo = parseInt(txtCode.value);
+
+    /* Acá hago una búsqueda  valor del input y si lo equivalente
+    al id del objeto del array, entonces obtengo el objeto del array. */
+    const select = customerList.find((customer) => customer.id === codigo);
+
+    /* Acá gracias al 'input codigo' podemos hacer un 'if';
+    es decir, una condición. Si el valor del input es equivalente
+    al parámetro de la lista , entonces, obtengo el objeto del array. */
+    if (select) {
+        //// door = true;
+        txtIc.value = select.ic;
+        txtNames.value = select.names;
+    } else {
+        alert('This customer is not in the list');
+    }
+});
+
+btnClear.addEventListener('click',()=>{
+    txtCode.value='';
+    txtIc.value='';
+    txtNames.value='';
+    txtKms.value='0.00';
+    txtCost.value='0.00';
+    txtPeople.value='';
+    txtSubtotal.value='0.00';
+    // txtIva.value='';
+    txtTotal.value='0.00';
+});
+
+btnCalculate.addEventListener('click',()=>{
+    
+});

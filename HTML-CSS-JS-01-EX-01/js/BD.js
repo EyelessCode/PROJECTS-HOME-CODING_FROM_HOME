be99e2ef7abed44e12666a90cfaaf990a7a2111a
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-12-01 10:33:50 -0500

    Update BD.js
    Here we have the properties, each of these have an id. There are two array.

---
+++ HTML-CSS-JS-01-EX-01/js/BD.js
@@ -0,0 +1,35 @@
+export const customerList = [
+    { id: 1, names: "Jhon Anibal Gonzáles Pinos", ic: "0974852514" },
+    { id: 2, names: "Juan Eduardo Carlos Contreras", ic: "0963589615" },
+    { id: 3, names: "Maria Isabel Rodriguez Lopez", ic: "0974852516" },
+    { id: 4, names: "Carlos Andres Martinez Salazar", ic: "0963589617" },
+    { id: 5, names: "Ana Patricia Jimenez Torres", ic: "0974852518" },
+    { id: 6, names: "Pedro Antonio Gomez Vasquez", ic: "0963589619" },
+    { id: 7, names: "Lucia Fernanda Lopez Perez", ic: "0974852520" },
+    { id: 8, names: "Sofia Maria Hernandez Castro", ic: "0963589621" },
+    { id: 9, names: "David Alejandro Jimenez Torres", ic: "0974852522" },
+    { id: 10, names: "Sandra Patricia Rodriguez Lopez", ic: "0963589623" },
+    { id: 11, names: "Jorge Luis Gonzáles Pinos", ic: "0974852524" },
+    { id: 12, names: "Carla Isabel Gomez Vasquez", ic: "0963589625" },
+    { id: 13, names: "Fernando Andres Martinez Salazar", ic: "0974852526" },
+    { id: 14, names: "Paola Fernanda Lopez Perez", ic: "0963589627" },
+    { id: 15, names: "Ricardo Antonio Gomez Vasquez", ic: "0974852528" }
+];
+
+export const sitieList = [
+    { id: 1, place: "Galápagos", travelInKm: 950, cost: 1500 },
+    { id: 2, place: "Quito", travelInKm: 420, cost: 300 },
+    { id: 3, place: "Baños de Agua Santa", travelInKm: 180, cost: 200 },
+    { id: 4, place: "Cuenca", travelInKm: 190, cost: 250 },
+    { id: 5, place: "Montañita", travelInKm: 200, cost: 150 },
+    { id: 6, place: "Cotopaxi", travelInKm: 150, cost: 100 },
+    { id: 7, place: "Otavalo", travelInKm: 120, cost: 80 },
+    { id: 8, place: "Mindo", travelInKm: 100, cost: 90 },
+    { id: 9, place: "Guayaquil", travelInKm: 310, cost: 250 },
+    { id: 10, place: "Salinas", travelInKm: 140, cost: 130 },
+    { id: 11, place: "Papallacta", travelInKm: 70, cost: 60 },
+    { id: 12, place: "Vilcabamba", travelInKm: 400, cost: 220 },
+    { id: 13, place: "Quilotoa", travelInKm: 180, cost: 180 },
+    { id: 14, place: "Puerto López", travelInKm: 290, cost: 170 },
+    { id: 15, place: "Ibarra", travelInKm: 110, cost: 90 }
+];

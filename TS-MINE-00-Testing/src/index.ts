56dbf75b846ad58be6188a3956adc39b623d65d1
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-12-05 21:28:05 -0500

    Update index.ts
    More things...

--- TS-MINE-00-Testing/src/index.ts
+++ TS-MINE-00-Testing/src/index.ts
@@ -77,16 +77,80 @@ enum Color {
 forma explícita. Cuando una propiedad tiene un '?'
 (signo de interrogación) es porque es opcional */
 const objetc:{
-    id:number,
+    /* El 'readonly' sirve para dejar la propiedad
+    como lectura, por lo que no podrás cambiar su valor */
+    readonly id:number,
     name:String,
     age?:number
+    tallas:talla
 }={
     id:1,
     name:'Juan',
     // age:30
+    tallas:talla.supergrande
 }
 
 /* También puedes cambiarle los valores de las propiedades.
 Algo que se puede hacer en TS y JS, pero lo que no se puede
 hacer en TS pero si es JS es agregarle una nueva propiedad. */
 objetc.name='Pedro'
+
+/* Podemos usar el tipo de propiedades en otro lado,
+no solo ahí mismo. */
+/* El 'type' sirve para definir un tipo de dato. Pero
+supongo que sirve para objetos. */
+type addressTwo={
+    street:String,
+    city:String,
+    country:String
+}
+
+type objetc2={
+    id:number,
+    names:String,
+    age?:number,
+    tallas:talla,
+    /* También podemos agregar objetos como propiedades */
+    addres:{
+        street:String,
+        number:number,
+        country:String
+    },
+    /* Pero también podemos hacer la misma mágia de llamar
+    al objeto desde afuera. */
+    adressTwo:addressTwo
+};
+
+const objetc2Value:objetc2={
+    id:1,
+    names:'Juan',
+    age:30,
+    tallas:talla.pequenia,
+    addres:{
+        street:'Falcón',
+        number:7,
+        country:'Holanda'
+    },
+    /* Objeto agregado por objeto agregado por objeto. */
+    adressTwo:{
+        street:'Falcón',
+        city:'Holanda',
+        country:'Holanda'
+    }
+}
+
+/* Podemos crear un arreglo o 'array' para objetos.
+Un ejemplo sería 'customer' o 'student' donde este
+tenga las propiedades de estos objetos; como:
+name, lastname, age, etc. */
+type studentData={
+    readonly id:number,
+    name:string,
+    lastname:string,
+    age?:number
+}
+const student:studentData[]=[
+    {id:1,name:'Juan',lastname:'Pérez'},
+    {id:2,name:'Juan',lastname:'Pérez'},
+    {id:3,name:'Juan',lastname:'Pérez'},
+]

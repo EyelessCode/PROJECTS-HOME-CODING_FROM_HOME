2c64eef3fe10f2f283e8b10abef1d18c8fc6a269
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-12-07 20:10:03 -0500

    Create BD.js

---
+++ JS-MINE-AGAIN-01-TestingWeb/js/BD.js
@@ -0,0 +1,87 @@
+
+
+export const mealList=[
+    {
+        id:1,
+        name:'Burger with chicken, meat, egg, tomato and French fries',
+        price:8.75,
+        description:'Burger with chicken, meat, egg, tomato and French fries menu',
+/*         aditional:{
+            name:'Extra cheese',
+            priceAditional:2.50
+        }
+ */    },
+    {
+        id:2,
+        name:'Large plate of rice with stew with a medium beef and 3 fried eggs',
+        price:9.75,
+        // description:'This is a test meal',
+/*         aditional:{
+            name:'Extra cheese',
+            priceAditional:2.50
+        }
+ */    },
+    {
+        id:3,
+        name:'Medium-sized pizza with pepperoni and more',
+        price:4.25,
+/*         aditional:{
+            name:'Extra epic cheese',
+            priceAditional:4.00
+        }
+ */    }
+]
+
+export const aditionalOneLis=[
+    {
+        id:1,
+        name:'Giant Coca Cola with refillable cup',
+        priceAditional:3.50
+    },
+    {
+        id:2,
+        name:'Big Cola Giant with refillable glass',
+        priceAditional:3.50
+    },
+    {
+        id:3,
+        name:'Giant orange, strawberry or blackberry flavored juice with refillable glass',
+        priceAditional:3.50
+    }
+]
+
+export const aditionalTwoList=[
+    {
+        id:1,
+        name:'2 medium fries and a medium drink',
+        priceAditional:4.75
+    },
+    {
+        id:2,
+        name:'A medium hamburger and a small drink',
+        priceAditional:4.75
+    },
+    {
+        id:3,
+        name:'2 medium hamburgers',
+        priceAditional:4.75
+    }
+]
+
+export const aditionalThreeList=[
+    {
+        id:1,
+        name:'An additional pizza with a medium drink',
+        priceAditional:4.25
+    },
+    {
+        id:2,
+        name:'3 small drinks',
+        priceAditional:4.25
+    },
+    {
+        id:3,
+        name:'2 small drinks and some small chips',
+        priceAditional:4.25
+    }
+]

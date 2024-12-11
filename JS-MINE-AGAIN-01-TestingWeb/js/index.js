92b04652a6c3a93cf41a1c8ce4933c6dad886f9c
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-12-11 12:31:14 -0500

    Edit in Style, Index.html and update in index.JS.
    In style and index.HTML, i just edit some part of the workspace.
    
    In index.JS, i added conditions, default options, and so on.

--- JS-MINE-AGAIN-01-TestingWeb/js/index.js
+++ JS-MINE-AGAIN-01-TestingWeb/js/index.js
@@ -5,6 +5,7 @@ const cmbMeal=document.getElementById('cmbMeal');
 const cmbAditional=document.getElementById('cmbAditional');
 const txtPrice=document.getElementById('txtPrice');
 const txtIva=document.getElementById('txtIva');
+const txtAditional=document.getElementById('txtAditional');
 const txtSubtotal=document.getElementById('txtSubtotal');
 const txtTotal=document.getElementById('txtTotal');
 const btnCalc=document.getElementById('btnCalc');
@@ -35,19 +36,126 @@ cmbMeal.addEventListener('change',()=>{
     let i=cmbMeal.selectedIndex;
     let code=cmbMeal.options[i].value;
     let mealSelection=mealList.find((meal)=>meal.id===parseInt(code));
+    // alert(`Índice: ${code} o ${mealSelection.id}`);
+
     let iva=ivaEnter(mealSelection.price).toFixed(2);
+
     
     txtIva.value=parseFloat(iva).toFixed(2);
     txtPrice.value=parseFloat(mealSelection.price).toFixed(2);
+    txtAditional.value='';
     // alert(`se está ejecunado...`);
+    switch(parseInt(code)){
+        case 1:
+            cmbAditional.innerHTML='';
+
+            // if(true){
+                let option=document.createElement('option');
+                option.value=''
+                option.textContent='-- ANY --'
+                option.selected=true;
+                option.disabled=false;
+                cmbAditional.appendChild(option);
+
+            // }
+            
+            aditionalOneLis.forEach((adOne)=>{
+                let option=document.createElement('option');
+                option.value=adOne.id;
+                option.textContent=adOne.name;
+                cmbAditional.appendChild(option);
+            });
+            
+            if(true){
+                cmbAditional.addEventListener('change',()=>{
+                    // alert(`hoal!`)
+                    // console.log(`hoal!`)
+                    let i=cmbAditional.selectedIndex;
+                    let code=cmbAditional.options[i].value;
+                    let aditionalSelect=aditionalOneLis.find((adOne)=>adOne.id===parseInt(code));
+                    // alert(`el es ${code} ${aditionalSelect.id}`)
+                });
+            }
+
+        break;
+        case 2:
+            cmbAditional.innerHTML='';
+
+            if(true){
+                let option=document.createElement('option');
+                option.value=''
+                option.textContent='-- ANY --'
+                option.selected=true;
+                option.disabled=false;
+                cmbAditional.appendChild(option);
+            }
+
+            aditionalTwoList.forEach((adTwo)=>{
+                let option=document.createElement('option');
+                option.value=adTwo.id;
+                option.textContent=adTwo.name;
+                cmbAditional.appendChild(option);
+            });
+
+            if(true){
+                cmbAditional.addEventListener('change',()=>{
+                    // alert(`hoal!`)
+                    // console.log(`hoal!`)
+                    let i=cmbAditional.selectedIndex;
+                    let code=cmbAditional.options[i].value;
+                    let aditionalSelect=aditionalTwoList.find((adTwo)=>adTwo.id===parseInt(code));
+
+                    let aditional=Number(aditionalSelect.priceAditional).toFixed(2);
+
+                    txtAditional.value=aditional;
+                });
+            }
+        break;
+        case 3:
+            cmbAditional.innerHTML='';
+
+            if(true){
+                let option=document.createElement('option');
+                option.value=''
+                option.textContent='-- ANY --'
+                option.selected=true;
+                option.disabled=false;
+                cmbAditional.appendChild(option);
+            }
+
+            aditionalThreeList.forEach((adThree)=>{
+                let option=document.createElement('option');
+                option.value=adThree.id;
+                option.textContent=adThree.name;
+                cmbAditional.appendChild(option);
+            });
+
+            if(true){
+                cmbAditional.addEventListener('change',()=>{
+                    // alert(`hoal!`)
+                    // console.log(`hoal!`)
+                    let i=cmbAditional.selectedIndex;
+                    let code=cmbAditional.options[i].value;
+                    let aditionalSelect=aditionalThreeList.find((adThree)=>adThree.id===parseInt(code));
+
+                    let aditional=Number(aditionalSelect.priceAditional).toFixed(2);
+
+                    txtAditional.value=aditional;
+                });
+            }
+        break;
+
+    }
 });
+
+
 
 // const buttonCalc=
 btnCalc.addEventListener('click',()=>{
     // console.log(`hoal`)
-/*     let i=cmbMeal.selectedIndex;
+    let i=cmbMeal.selectedIndex;
     let code=cmbMeal.options[i].value;
-    let mealSelection=mealList.find((meal)=>meal.id===parseInt(code)); */
+    let mealSelection=mealList.find((meal)=>meal.id===parseInt(code));
     
     // let price=parseFloat(mealSelection.price).toFixed(2);
     let price=Number(txtPrice.value);
@@ -63,39 +171,26 @@ btnCalc.addEventListener('click',()=>{
         txtTotal.value=parseFloat(total).toFixed(2);
         // console.log(`El precio: ${price} el iva: ${ivaa} el subtotal: ${subtotal} el total: ${total}`);
 
-        if(subtotal!==0){
-            cmbAditional.addEventListener('change',()=>{
-            })
-        }
     }else{
         alert(`Select a meal to calculate the total price`);
     }
 });
 
 
-
-/* txtPrice.addEventListener('input',()=>{
-    let i=cmbMeal.selectedIndex
-    let code=cmbMeal.options[i].value
-    let mealSelection=mealList.find((meal)=>meal.id===parseInt(code));
-
-    if (txtPrice.value>1) {
-        btnCalc.disabled=false;
-        alert('hoal')
-        btnCalc.addEventListener('click',()=>{
-            
-        });
-    }else{
-        btnCalc.disabled=true;
-    }
-
-}); */
-
 btnClean.addEventListener('click',()=>{
-    cmbMeal.value='';
-    cmbAditional.value='';
+    cmbMeal.selectedIndex=0
+    
+    cmbAditional.innerHTML='';
+    let defaultOption=document.createElement('option');
+    defaultOption.value=''
+    defaultOption.textContent='-- SELECT AN OPTION --'
+    defaultOption.selected=true;
+    defaultOption.disabled=true;
+    cmbAditional.appendChild(defaultOption);
+
     txtPrice.value='';
     txtIva.value='';
     txtSubtotal.value='';
     txtTotal.value='';
-})
+    txtAditional.value='';
+});

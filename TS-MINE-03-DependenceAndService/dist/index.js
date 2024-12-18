01303ef163ad104f01f6b942846cfd41fb9a796e
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-12-18 17:51:37 -0500

    Update in index.TS.
    Transpilation. I added a server with a PORT, i don't know yet how it works.

--- TS-MINE-03-DependenceAndService/dist/index.js
+++ TS-MINE-03-DependenceAndService/dist/index.js
@@ -1,3 +1,9 @@
-"use strict";
-alert(`Hola mundo puta perra`);
+import express from 'express';
+const app = express();
+const PORT = 4000;
+app.use(express.json());
+app.get('/', (req, res) => {
+    res.send("Hello World!");
+});
+app.listen(PORT, () => console.log(`Server listening in PORT number: ${PORT}`));
 //# sourceMappingURL=index.js.map
--- TS-MINE-03-DependenceAndService/src/index.ts
+++ TS-MINE-03-DependenceAndService/src/index.ts
@@ -1,2 +1,11 @@
-alert(`Hola mundo puta perra`);
+// alert(`Hola mundo puta perra`);
 import express, {Response,Request} from 'express';
+
+const app=express();
+const PORT=4000;
+
+app.use(express.json());
+app.get('/',(req:Request,res:Response)=>{
+    res.send("Hello World!");
+});
+app.listen(PORT,()=>console.log(`Server listening in PORT number: ${PORT}`))

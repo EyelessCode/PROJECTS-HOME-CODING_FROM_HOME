f7dc50aa21995b54afc4b4e04e3d480da28a8809
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-12-20 17:23:10 -0500

    Update index.ts

--- TS-MINE-03-DependenceAndService/src/index.ts
+++ TS-MINE-03-DependenceAndService/src/index.ts
@@ -8,4 +8,4 @@ app.use(express.json());
 app.get('/',(req:Request,res:Response)=>{
     res.send("Hello World!");
 });
-app.listen(PORT,()=>console.log(`Server listening in PORT number: ${PORT}`))
+app.listen(PORT,()=>console.log(`Server listening in PORT number: ${PORT}`));

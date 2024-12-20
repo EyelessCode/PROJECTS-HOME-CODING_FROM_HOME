c3df564b2dea0f7f3e5ba10f1d1742ac8d110661
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-12-20 18:35:36 -0500

    Transpilation.
    I make a transpilation files.

---
+++ TS-MINE-04-ServerWithHTML/dist/index.js
@@ -0,0 +1,6 @@
+import express from 'express';
+const app = express();
+const PORT = 4000;
+app.use(express.static('public'));
+app.listen(PORT, () => console.log(`Listening server in: ${PORT}`));
+//# sourceMappingURL=index.js.map

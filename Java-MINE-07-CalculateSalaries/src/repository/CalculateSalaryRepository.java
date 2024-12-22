9b90ac7a0b5958ec69fe6a37e5d23298491c22ff
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-12-21 23:35:52 -0500

    Update in the repository class.
    I added the methods, first it will calculate the salary and plus iva. Second a salary fixed and third it will calculate salary and take off with discount.

--- Java-MINE-07-CalculateSalaries/src/repository/CalculateSalaryRepository.java
+++ Java-MINE-07-CalculateSalaries/src/repository/CalculateSalaryRepository.java
@@ -1,5 +1,34 @@
 package repository;
 
+import model.Employee;
+
 public class CalculateSalaryRepository {
-    
+    private Employee employee=new Employee(null, 0.00);
+
+    public double repositoryWorker(){
+        employee.setWork("Repository".toLowerCase());
+        employee.setSalary(15_890);
+        double bonus=0.10;
+        double iva=employee.getSalary()*bonus;
+        return employee.getSalary()+iva;
+        // return 0;
+    }
+
+    public double atmWorker(){
+        employee.setWork("ATM".toLowerCase());
+        employee.setSalary(25_630.89);
+        // double bonus=0.10;
+        // double iva=employee.getSalary()*bonus;
+        return employee.getSalary();
+        // return 0;
+    }
+
+    public double supervisorWorker(){
+        employee.setWork("Supervisor".toLowerCase());
+        employee.setSalary(35_560.20);
+        double discount=0.11;
+        double iva=employee.getSalary()*discount;
+        return employee.getSalary()-iva;
+        // return 0;
+    }
 }

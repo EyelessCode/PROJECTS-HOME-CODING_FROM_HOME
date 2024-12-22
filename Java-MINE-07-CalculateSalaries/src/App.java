2e2b2d1f564b2d0d650fa14fd617ead7e280edb8
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-12-21 23:36:23 -0500

    Update in App.
    I call the Class and do a print.

--- Java-MINE-07-CalculateSalaries/src/App.java
+++ Java-MINE-07-CalculateSalaries/src/App.java
@@ -1,5 +1,10 @@
+import repository.CalculateSalaryRepository;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        CalculateSalaryRepository calculateRepository=new CalculateSalaryRepository();
         System.out.println("Hello, World!");
+        System.out.printf("R: %.2f - A: %.2f - S: %.2f", calculateRepository.repositoryWorker(),
+        calculateRepository.atmWorker(),calculateRepository.supervisorWorker());
     }
 }

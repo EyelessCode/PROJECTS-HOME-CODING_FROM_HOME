c34a1d2ed90e6053d6a4c5a8328769b1eecd5ce9
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-12-22 13:34:25 -0500

    Update in the controller.
    I change the method, before was an integer method, now it's a void method. It's for the method can't repeat.

--- Java-MINE-07-CalculateSalaries/src/controller/SalaryController.java
+++ Java-MINE-07-CalculateSalaries/src/controller/SalaryController.java
@@ -1,7 +1,19 @@
 package controller;
 
+import repository.ValidationRepository;
+import view.WorkerView;
+
 public class SalaryController {
-    public void pickWorker(){
-        
+    private ValidationRepository validationRepository=new ValidationRepository();
+    private WorkerView workerView=new WorkerView();
+    // private int input;
+    
+    public void validationWorker(){
+        // lineString();
+        workerView.pickWorker();
+        validationRepository.validationPick();
+        // return validationWorker();
     }
+
+
 }

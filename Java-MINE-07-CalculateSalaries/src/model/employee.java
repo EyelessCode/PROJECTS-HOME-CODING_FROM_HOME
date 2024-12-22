b6cab363c9683cb0c00bb19532347c97e8139e2d
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-12-21 22:22:19 -0500

    Rename in the Employee model.

--- Java-MINE-07-CalculateSalaries/src/model/employee.java
+++ Java-MINE-07-CalculateSalaries/src/model/employee.java
@@ -1,5 +1,34 @@
 package model;
 
-public class employee {
+public class Employee {
+    private String work;
+    private int salary;
+    
+    public Employee(String work, int salary) {
+        this.work = work;
+        this.salary = salary;
+    }
 
+    public String getWork() {
+        return work;
+    }
+
+    public void setWork(String work) {
+        this.work = work;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "employee [work=" + work + ", salary=" + salary + "]";
+    }
+
+    
 }

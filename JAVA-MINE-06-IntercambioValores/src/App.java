de491a21a2b7c8d7924156a22b4c56dffb2e7314
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-12-21 19:06:36 -0500

    Create new project.
    I did a logic exercise, a variable with a number and other variable with other number, they will need to change its value to the other, then print to the console.

---
+++ JAVA-MINE-06-IntercambioValores/src/App.java
@@ -0,0 +1,9 @@
+import repository.ChangeValue;
+
+public class App {
+    private ChangeValue cv=new ChangeValue();
+    public void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        cv.change();
+    }
+}

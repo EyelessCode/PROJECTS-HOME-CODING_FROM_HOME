d3489e2db6c1d39d812668270d58e762aeea082a
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-12-23 17:48:38 -0500

    Update in the App.
    I call the method from repository. Then it show it.

--- Java-MINE-08-VectorCharge/src/App.java
+++ Java-MINE-08-VectorCharge/src/App.java
@@ -1,5 +1,13 @@
+import repository.VectorialChargeRespoitory;
+// import view.VectorView;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        VectorialChargeRespoitory vectorialChargeRespoitory=new VectorialChargeRespoitory();
+        // VectorView vectorView=new VectorView();
+
         System.out.println("Hello, World!");
+        // System.out.println(vectorView.lineString());
+        vectorialChargeRespoitory.vectorFunction();
     }
 }

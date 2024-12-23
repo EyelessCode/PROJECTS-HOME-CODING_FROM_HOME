d5119639bf2169910684d5181e8e0c3bae244afe
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-12-23 16:27:03 -0500

    Update in view class.
    Now it can show in the console.

--- Java-MINE-08-VectorCharge/src/view/VectorView.java
+++ Java-MINE-08-VectorCharge/src/view/VectorView.java
@@ -1,5 +1,14 @@
 package view;
 
 public class VectorView {
-    
+    public String lineString(){
+        String LINE="=".repeat(30);
+        return LINE;
+    }
+
+    public void draw() {
+        lineString();
+        System.out.println("Drawing a vector"+
+        "\n↓"+"\n↓"+"\n↓"+"\n↓"+"\n↓");
+    }
 }

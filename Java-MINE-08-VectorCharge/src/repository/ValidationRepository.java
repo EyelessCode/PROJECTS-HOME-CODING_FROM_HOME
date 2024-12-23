cbfc44e5854bf401cf42a37428d20a44387bb8a3
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-12-23 16:46:22 -0500

    Update in the validation repository.
    I added a method that you can input only a number not another thing.

--- Java-MINE-08-VectorCharge/src/repository/ValidationRepository.java
+++ Java-MINE-08-VectorCharge/src/repository/ValidationRepository.java
@@ -1,5 +1,22 @@
 package repository;
 
+import java.util.Scanner;
+
+import view.VectorView;
+
 public class ValidationRepository {
-    
+    private Scanner scanner = new Scanner(System.in);
+    private VectorView vectorView = new VectorView();
+
+    public int inputvalidation(){
+        int input = -1;
+        
+        while (!scanner.hasNextInt()) {
+            System.out.print("Enter number: ");
+            vectorView.draw();
+            scanner.next();
+        }
+        input=scanner.nextInt();
+        return input;
+    }
 }

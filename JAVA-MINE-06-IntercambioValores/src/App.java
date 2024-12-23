bd3c5faca1d008d714c7377b6e187350fa5d801e
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-12-23 15:32:27 -0500

    I fixing it.

--- JAVA-MINE-06-IntercambioValores/src/App.java
+++
@@ -1,9 +0,0 @@
-import repository.ChangeValue;
-
-public class App {
-    private ChangeValue cv=new ChangeValue();
-    public void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        cv.change();
-    }
-}

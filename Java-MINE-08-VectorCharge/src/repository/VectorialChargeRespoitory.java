3842742038c12fa7ab6665b93cfdbc83a1df13cb
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-12-23 17:47:59 -0500

    Update in to the vectorial repository.
    I added a method that you input a number for 'x' times and then show you in the console, if those number are odd or even and how many the number three repeat.

--- Java-MINE-08-VectorCharge/src/repository/VectorialChargeRespoitory.java
+++ Java-MINE-08-VectorCharge/src/repository/VectorialChargeRespoitory.java
@@ -1,5 +1,35 @@
 package repository;
 
+import view.VectorView;
+
 public class VectorialChargeRespoitory {
-    
+    private ValidationRepository validationRepository=new ValidationRepository();
+    private VectorView vectorView = new VectorView();
+
+    public void vectorFunction(){
+        int vector[]=new int[15];
+
+        vectorView.draw();
+        for (int i = 0; i < vector.length; i++) {
+            System.out.print("Enter a number: ");
+            vector[i]=validationRepository.inputvalidation();
+            System.out.printf("\nNumber: %s",vector[i]);
+            System.out.println("\nYour in the turn "+i+"\n");
+        }
+
+        int sum = 0;
+        for (int i = 0; i < vector.length; i++) {
+            if(vector[i]%2==1){ // if the number is odd
+                System.out.println("\nThe number "+vector[i]+" that you entered is odd");
+            }else{
+                System.out.println("\nThe number "+vector[i]+" that you entered is even");
+            }
+
+            if (vector[i]==3) {
+                sum++;
+            }
+        }
+
+        System.out.println("The number '3' is repeanting "+sum+" times!");
+    }
 }

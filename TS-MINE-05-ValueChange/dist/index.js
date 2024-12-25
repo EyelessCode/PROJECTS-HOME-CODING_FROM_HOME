0a8298c7182a71bf05a229685b144bb1a927add0
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-12-25 15:24:07 -0500

    Transpilation done.
    Transpilation done.

---
+++ TS-MINE-05-ValueChange/dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+let x = 30;
+let y = 27;
+let temp;
+console.log(`Value before the change x=${x} and y=${y}`);
+temp = x;
+x = y;
+y = temp;
+console.log(`Value after the change x=${x} and y=${temp}`);
+//# sourceMappingURL=index.js.map

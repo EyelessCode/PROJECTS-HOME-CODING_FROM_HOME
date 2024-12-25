42c84d16d24a4fa1cb3717ec7be84aa680bc0a84
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-12-25 18:54:40 -0500

    Update worker.model.ts
    Model done as well.

---
+++ TS-MINE-06-CalculateSalaries/src/model/worker.model.ts
@@ -0,0 +1,10 @@
+export class WorkerModel {
+    constructor(public work: string, public salary: number) {
+        this.work = work;
+        this.salary = salary;
+    }
+
+    toString(): string {
+        return `Employee: ${this.work}, Salary: ${this.salary}`;
+    }
+}

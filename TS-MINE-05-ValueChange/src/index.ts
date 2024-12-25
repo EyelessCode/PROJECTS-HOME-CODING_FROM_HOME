0d29c272623492bc54007d6b3f1067105d7c54e8
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-12-25 15:32:32 -0500

    Format the code

--- TS-MINE-05-ValueChange/src/index.ts
+++ TS-MINE-05-ValueChange/src/index.ts
@@ -1,10 +1,9 @@
-
-let x:number=30
-let y: number = 27
-let temp:number;
+let x: number = 30;
+let y: number = 27;
+let temp: number;
 console.log(`Value before the change x=${x} and y=${y}`);
 
-temp=x;
-x=y;
-y=temp;
+temp = x;
+x = y;
+y = temp;
 console.log(`Value after the change x=${x} and y=${temp}`);

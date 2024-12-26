337c1365137ff8fe384458d7898fccabf166ca31
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-12-25 21:32:46 -0500

    Update loadDinamic.controller.ts
    I still working...

---
+++ TS-MINE-06-CalculateSalaries/public/ts/controller/loadDinamic.controller.ts
@@ -0,0 +1,13 @@
+import {repositoryObject} from '../data/typeWorker.data'
+const cmbWorker=document.getElementById('cmbWorker') as HTMLSelectElement;
+const txtSalary=document.getElementById('txtSalary') as HTMLInputElement;
+const txtBonusOrDiscount=document.getElementById('txtBonusOrDiscount') as HTMLInputElement;
+const txtTotal=document.getElementById('txtTotal') as HTMLInputElement;
+const btncalculate=document.getElementById('btncalculate') as HTMLButtonElement;
+const btnDelete=document.getElementById('btnDelete') as HTMLButtonElement;
+
+cmbWorker.addEventListener('click',()=>{
+    repositoryObject.forEach((xd)=>{
+
+    });
+});

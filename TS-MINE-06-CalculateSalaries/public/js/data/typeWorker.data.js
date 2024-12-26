9f1e0b606aae1ed0565af64f6cda9a18101a7b05
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-12-25 21:51:26 -0500

    Create typeWorker.data.js

---
+++ TS-MINE-06-CalculateSalaries/public/js/data/typeWorker.data.js
@@ -0,0 +1,22 @@
+/* type property={
+    worker:String,
+    salary:number
+} */
+
+export const workerList=[
+    {
+        id:1,
+        worker:"repository",
+        salary:15_890
+    },
+    {
+        id:2,
+        worker:"atm",
+        salary:25_630.89
+    },
+    {
+        id:3,
+        worker:"supervisor",
+        salary:35_560.20
+    }
+]

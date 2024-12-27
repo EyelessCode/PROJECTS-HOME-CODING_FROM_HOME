b49103949be4866e485c2704ce7536e40d50b20c
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-12-27 12:52:06 -0500

    transpilation.
    transpilation

--- TS-MINE-06-CalculateSalaries/dist/index.js
+++ TS-MINE-06-CalculateSalaries/dist/index.js
@@ -1,3 +1,6 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const calculateSalary_repository_1 = require("./repository/calculateSalary.repository");
 console.log(`Index.ts has been conected!`);
+console.log(calculateSalary_repository_1.CalculateSalary.toString());
 //# sourceMappingURL=index.js.map
--- TS-MINE-06-CalculateSalaries/src/index.ts
+++ TS-MINE-06-CalculateSalaries/src/index.ts
@@ -1,1 +1,4 @@
+import { CalculateSalary } from './repository/calculateSalary.repository';
+
 console.log(`Index.ts has been conected!`)
+console.log(CalculateSalary.toString());

5476206dbbbff20cf9a69878c896628265b2efe3
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-12-31 17:07:20 -0500

    Update app.ts

--- TS-07-MINE-MastersCopy/src/app.ts
+++ TS-07-MINE-MastersCopy/src/app.ts
@@ -1,11 +1,13 @@
 import express, { Request, Response } from 'express';
+import RoutClient from './routes/client.routes';
 
 const app=express();
 const PORT=4000;
 
 app.use(express.json())
-app.get('/api',(req: Request,res: Response)=>{
+app.get('/',(req: Request,res: Response)=>{
     res.send(`Hola tu putamadre!`);
 });
+app.use('/api',RoutClient)
 
 app.listen(PORT,()=>console.log(`Your server was connected in the ${PORT} PORT!`))

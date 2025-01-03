d8de0eafbe67c5051befff07d2ec0409290c76bd
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-02 22:48:31 -0500

    Update app.ts

--- TS-MINE-07-FollowingTuto-ApiRest/src/app.ts
+++ TS-MINE-07-FollowingTuto-ApiRest/src/app.ts
@@ -6,6 +6,7 @@ import { router } from "./router"
 const PORT=process.env.PORT||4000
 const app=express()
 app.use(cors())
+app.use(express.json())
 app.use(router)
 
-app.listen(PORT,()=>console.log(`Listenning in ${PORT}`))
+app.listen(PORT,()=>console.log(`Listenning in '${PORT}' PORT`))

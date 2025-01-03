f6e7d65740c67d0800bef271d17f8e1b6a6ff503
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-02 20:34:40 -0500

    Update item.router.ts

--- TS-MINE-07-FollowingTuto-ApiRest/src/router/item.router.ts
+++ TS-MINE-07-FollowingTuto-ApiRest/src/router/item.router.ts
@@ -2,10 +2,10 @@ import { Request, Response, Router } from "express";
 
 const router=Router()
 
-router.get('/item',(req:Request,res:Response)=>{
+router.get('/',(req:Request,res:Response)=>{
     res.send({
         data:"MODELS_HERE"
     })
 })
 
-export default router;
+export {router};

3f0d36ed0b2c3f7087388a12f879bb6f72b521ec
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-02 20:34:36 -0500

    Update app.ts

--- TS-MINE-07-FollowingTuto-ApiRest/src/app.ts
+++ TS-MINE-07-FollowingTuto-ApiRest/src/app.ts
@@ -1,9 +1,11 @@
 import "dotenv/config"
 import express from 'express'
 import cors from 'cors'
+import { router } from "./router"
 
 const PORT=process.env.PORT||4000
 const app=express()
 app.use(cors())
+app.use(router)
 
 app.listen(PORT,()=>console.log(`Listenning in ${PORT}`))

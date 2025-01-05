a15814952138467de613f7e310d4b66f205fd65a
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-04 21:00:28 -0500

    Update app.ts

--- TS-MINE-07-FollowingTuto-ApiRest/src/app.ts
+++ TS-MINE-07-FollowingTuto-ApiRest/src/app.ts
@@ -2,7 +2,7 @@ import "dotenv/config"
 import express from 'express'
 import cors from 'cors'
 import { router } from "./router"
-import dbPostgreSql from './config/oracleBD'
+import dbPostgreSql from './config/postgreSql'
 
 const PORT=process.env.PORT||4000
 const app=express()

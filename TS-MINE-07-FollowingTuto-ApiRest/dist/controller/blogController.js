cc8fec583c29519b86649c932a016a2e6772c064
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-05 18:06:58 -0500

    Transpilation

--- TS-MINE-07-FollowingTuto-ApiRest/dist/controller/blogController.js
+++ TS-MINE-07-FollowingTuto-ApiRest/dist/controller/blogController.js
@@ -11,7 +11,6 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.deleteItem = exports.createItem = exports.update = exports.getItems = exports.getItem = void 0;
 const error_handle_1 = require("../util/error.handle");
-const itemService_1 = require("../service/itemService");
 const getItem = (req, res) => {
     try {
     }
@@ -38,11 +37,9 @@ const update = (req, res) => {
 exports.update = update;
 const createItem = (_a, res_1) => __awaiter(void 0, [_a, res_1], void 0, function* ({ body }, res) {
     try {
-        const responseItem = yield (0, itemService_1.insertItem)(body);
-        res.send(responseItem);
     }
     catch (error) {
-        (0, error_handle_1.handleHttp)(res, 'ERROR_CREATE_BLOG');
+        (0, error_handle_1.handleHttp)(res, 'ERROR_CREATE_BLOG', error);
     }
 });
 exports.createItem = createItem;
--- TS-MINE-07-FollowingTuto-ApiRest/dist/controller/itemController.js
+++ TS-MINE-07-FollowingTuto-ApiRest/dist/controller/itemController.js
@@ -1,7 +1,17 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.deleteItem = exports.createItem = exports.update = exports.getItems = exports.getItem = void 0;
 const error_handle_1 = require("../util/error.handle");
+const itemService_1 = require("../service/itemService");
 const getItem = (req, res) => {
     try {
     }
@@ -26,14 +36,15 @@ const update = (req, res) => {
     }
 };
 exports.update = update;
-const createItem = ({ body }, res) => {
+const createItem = (_a, res_1) => __awaiter(void 0, [_a, res_1], void 0, function* ({ body }, res) {
     try {
-        res.send(body);
+        const responseItem = yield (0, itemService_1.insertItem)(body);
+        res.send(responseItem);
     }
     catch (error) {
-        (0, error_handle_1.handleHttp)(res, 'ERROR_CREATE_ITEM');
+        (0, error_handle_1.handleHttp)(res, 'ERROR_CREATE_ITEM', error);
     }
-};
+});
 exports.createItem = createItem;
 const deleteItem = (req, res) => {
     try {

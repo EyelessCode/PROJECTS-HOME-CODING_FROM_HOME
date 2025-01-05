df4904fe20c49eeda62eec3dfb3c2ff440f5f9b3
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-04 19:48:26 -0500

    Transpilation

---
+++ TS-MINE-07-FollowingTuto-ApiRest/dist/controller/itemController.js
@@ -0,0 +1,49 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.deleteItem = exports.postItem = exports.update = exports.getItems = exports.getItem = void 0;
+const getItem = (req, res) => {
+    try {
+    }
+    catch (error) {
+        res.status(500);
+        res.send('EROR_GET_ITEM');
+    }
+};
+exports.getItem = getItem;
+const getItems = (req, res) => {
+    try {
+    }
+    catch (error) {
+        res.status(500);
+        res.send('EROR_GET_ITEM');
+    }
+};
+exports.getItems = getItems;
+const update = (req, res) => {
+    try {
+    }
+    catch (error) {
+        res.status(500);
+        res.send('EROR_GET_ITEM');
+    }
+};
+exports.update = update;
+const postItem = (req, res) => {
+    try {
+    }
+    catch (error) {
+        res.status(500);
+        res.send('EROR_GET_ITEM');
+    }
+};
+exports.postItem = postItem;
+const deleteItem = (req, res) => {
+    try {
+    }
+    catch (error) {
+        res.status(500);
+        res.send('EROR_GET_ITEM');
+    }
+};
+exports.deleteItem = deleteItem;
+//# sourceMappingURL=itemController.js.map
---
+++ TS-MINE-07-FollowingTuto-ApiRest/dist/util/error.handle.js
@@ -0,0 +1,2 @@
+"use strict";
+//# sourceMappingURL=error.handle.js.map

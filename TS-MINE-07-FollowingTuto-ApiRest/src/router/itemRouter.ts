45175e4ba73eb0b4bd6b8aa035ce79987f62f1de
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-04 20:22:23 -0500

    Update itemRouter.ts

--- TS-MINE-07-FollowingTuto-ApiRest/src/router/itemRouter.ts
+++ TS-MINE-07-FollowingTuto-ApiRest/src/router/itemRouter.ts
@@ -1,11 +1,18 @@
 import { Request, Response, Router } from "express";
+import { createItem, deleteItem, getItem, getItems, update } from "../controller/itemController";
 
 const router=Router()
 
-router.get('/',(req:Request,res:Response)=>{
+/* router.get('/',(req:Request,res:Response)=>{
     res.send({
         data:"MODELS_HERE"
     })
-})
+}) */
+
+    router.get('/',getItems)
+    router.get('/:id',getItem)
+    router.post('/',createItem)
+    router.put('/:id',update)
+    router.delete('/:id',deleteItem)
 
 export {router};

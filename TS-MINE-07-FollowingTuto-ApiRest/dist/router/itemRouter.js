eebe49c3e2395a297ebc49a5b4d5b44699b1c132
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-04 20:56:56 -0500

    Transpilation

--- TS-MINE-07-FollowingTuto-ApiRest/dist/router/itemRouter.js
+++ TS-MINE-07-FollowingTuto-ApiRest/dist/router/itemRouter.js
@@ -5,9 +5,9 @@ const express_1 = require("express");
 const itemController_1 = require("../controller/itemController");
 const router = (0, express_1.Router)();
 exports.router = router;
-router.get('/', itemController_1.getItems);
-router.get('/:id', itemController_1.getItem);
-router.post('/', itemController_1.createItem);
-router.put('/:id', itemController_1.update);
-router.delete('/:id', itemController_1.deleteItem);
+router.get("/", itemController_1.getItems);
+router.get("/:id", itemController_1.getItem);
+router.post("/", itemController_1.createItem);
+router.put("/:id", itemController_1.update);
+router.delete("/:id", itemController_1.deleteItem);
 //# sourceMappingURL=itemRouter.js.map

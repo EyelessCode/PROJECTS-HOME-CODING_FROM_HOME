90bd1f124a253240610bb8561e1b54e4aa3cd2d9
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-11 18:10:45 -0500

    Update app.ts

--- TS-MINE-08-BackendAndFrontend/src/app.ts
+++ TS-MINE-08-BackendAndFrontend/src/app.ts
@@ -1,10 +1,20 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 import 'dotenv/config'
+import { fileURLToPath } from 'url'
+import path from 'path'
+
+//// const filename=fileURLToPath(import.meta.url)
+//// const dirname=path.dirname(filename)
 
 const app=express()
 const PORT=process.env.PORT||4001
 
 app.use(express.json())
-app.use(express.static('public/view'))
+// app.use(express.static('public/view'))
+app.use(express.static(path.join(__dirname,'public')))
+
+app.get('/',(req:Request,res:Response)=>{
+    res.sendFile(path.join(__dirname,'view','vitalSign.html'))
+})
 
 app.listen(PORT,()=>console.log(`SERVER LISTENING IN ${PORT}`))

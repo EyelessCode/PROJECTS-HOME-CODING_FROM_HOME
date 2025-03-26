f816ff5f002ea01d7dc92b56e8a3ddd9d51cfd9f
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-03-26 17:44:40 -0500

    Update app.js

--- HTML-LEARNING-04-Portafolios/app.js
+++ HTML-LEARNING-04-Portafolios/app.js
@@ -6,14 +6,16 @@ require('dotenv').config()
 const app=express()
 const PORT=process.env.PORT||4100
 
+const ENDPOINT_PORTAFOLIO="/portafolio"
+
 app.use(express.json())
 app.use(cors())
 app.use(express.static(path.join(__dirname,'public'))) //? Coge la carpeta como estática
 
-app.get('/portafolio',(req,res)=>{
+app.get(`${ENDPOINT_PORTAFOLIO}`,(req,res)=>{
     res.sendFile(path.join(__dirname,'view','index.html'))
 })
 
 app.listen(PORT,()=>{
-    console.log(`ESCUCHANDO EN EL PUERTO: ${PORT}`)
+    console.log(`ESCUCHANDO EN LA URL: localhost:${PORT}${ENDPOINT_PORTAFOLIO}`)
 })

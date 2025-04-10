5ad26f9a5cd7942186306602a206bae45810065d
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-04-10 10:33:27 -0500

    Update app.ts

--- HTML-LEARNING-05-GRID_TRY_RESPONSIVE/app.ts
+++ HTML-LEARNING-05-GRID_TRY_RESPONSIVE/app.ts
@@ -1,12 +1,18 @@
 import express, { Request, Response } from "express";
 import 'dotenv/config'
 import cors from "cors"
+import path from "path";
 
 const app=express()
 const PORT=process.env.PORT||2000
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get("/travels",(req:Request,res:Response)=>{
+    res.sendFile(path.join(__dirname, 'public', 'view', 'index.html'))
+})
 
 //! TEST
 app.get("/grid-R",(req:Request,res:Response)=>{

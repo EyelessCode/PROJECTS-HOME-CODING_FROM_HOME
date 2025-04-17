fd92c4edd0f58789c5e4bc5e359fb2855b5aea33
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-04-17 16:05:41 -0500

    Update script.js

--- HTML-LEARNING-05-GRID_TRY_RESPONSIVE/public/js/script.js
+++ HTML-LEARNING-05-GRID_TRY_RESPONSIVE/public/js/script.js
@@ -1,4 +1,21 @@
 window.addEventListener("scroll",function(){
-    let header=this.document.querySelector("header")
-    header.classList.toggle("downScroll",this.window.scrollY>0)
+    let header=this.document.querySelector(".header")
+    let logImg=this.document.querySelector(".logo a img")
+
+    if (this.window.scrollY>0) {
+        header.style.position="fixed"
+        header.style.top="0"
+        header.style.width="100%"
+        header.style.transform = "translateY(-10%)"
+        logImg.style.width="50px"
+        logImg.style.height="50px"
+    } else {
+        header.style.position="relative"
+        header.style.transform="translateY(0)"
+        header.style.backgroundColor="#2E5E3B"
+        header.style.width="100%"
+        logImg.style.width="100%"
+        logImg.style.height="150px"
+    }
+
 })

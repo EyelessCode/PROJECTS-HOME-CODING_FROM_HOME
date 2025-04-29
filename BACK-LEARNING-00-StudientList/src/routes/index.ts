63e627fd001e40e8b25a8128f62ab7cd0181ab22
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-04-29 11:41:20 -0500

    Update index.ts

---
+++ BACK-LEARNING-00-StudientList/src/routes/index.ts
@@ -0,0 +1,21 @@
+import { Router } from "express";
+import { readdirSync } from "fs";
+
+const ruta=Router()
+const PATH=`${__dirname}`
+
+const cortar=(file:string)=>{
+    const cortarFile=file.split('.').shift()
+    return cortarFile
+}
+
+readdirSync(PATH).filter((file)=>{
+    const clean=cortar(file)
+
+    if (clean!=='index') {
+        import (`./${clean}`).then((modulo)=>{
+            console.log(`Ruta: ${clean}`);
+            ruta.use(`/${clean}`,modulo.ruta)
+        })
+    }
+})

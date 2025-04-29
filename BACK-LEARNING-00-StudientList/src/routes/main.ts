14e230d8972b62c8e3c1349fddfbbd724c2ec35a
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-04-29 12:07:22 -0500

    Update main.ts

--- BACK-LEARNING-00-StudientList/src/routes/main.ts
+++ BACK-LEARNING-00-StudientList/src/routes/main.ts
@@ -1,6 +1,6 @@
 import { Request, Response, Router } from "express";
 
 export const ruta=Router()
-ruta.get('/main',(req:Request,res:Response)=>{
+ruta.get("/main",(req:Request,res:Response)=>{
     res.send(`Hola`)
 })

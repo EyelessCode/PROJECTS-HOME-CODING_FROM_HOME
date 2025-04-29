93e09a8cf5d28f04f30dbd9d5c01031c788aba1d
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-04-29 11:49:49 -0500

    Update app.ts

--- BACK-LEARNING-00-StudientList/src/app.ts
+++ BACK-LEARNING-00-StudientList/src/app.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from 'cors';
 import 'dotenv/config'
+import { ruta } from "./routes";
 
 const app=express()
 const PORT=process.env.PORT
 
 app.use(cors())
 app.use(express.json())
+app.use('test/',ruta)
 
 app.listen(PORT,()=>console.log(`=> ${PORT}`))

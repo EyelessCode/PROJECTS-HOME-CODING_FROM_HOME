4419b20bcb65dc9bf23573939904783d104beedb
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-04-30 19:59:02 -0500

    Update idStudent.ts

---
+++ BACK-LEARNING-00-StudientList/src/estudiante/domain/validation/model/idStudent.ts
@@ -0,0 +1,14 @@
+export class IdStudent{
+    id:number
+
+    constructor(id:number) {
+        this.id=id
+        this.numNegativo()
+    }
+
+    private numNegativo(){
+        if (this.id<0) {
+            throw new Error(`El ID no puede ser menor a '0'!`)
+        }
+    }
+}

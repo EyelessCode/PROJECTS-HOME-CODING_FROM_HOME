debe43a9296a739194976f90bae36768fe2d9a82
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-04-30 21:33:31 -0500

    Update repositoryStudent.interface.ts

---
+++ BACK-LEARNING-00-StudientList/src/estudiante/domain/interface/repository/repositoryStudent.interface.ts
@@ -0,0 +1,9 @@
+import { IdStudent } from '../../validation/model/idStudent.value';
+import { ClassEstudiante } from '../model/estudiante.model';
+export interface RepositoryEstudiante{
+    getAll():Promise<ClassEstudiante[]>
+    getOneById(id:IdStudent):Promise<ClassEstudiante|null>
+    create(student:ClassEstudiante):Promise<void>
+    edit(student:ClassEstudiante):Promise<void>
+    delete(id:IdStudent):Promise<void>
+}

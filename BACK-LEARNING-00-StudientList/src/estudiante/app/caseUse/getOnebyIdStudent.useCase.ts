04eba53d88682da1051967979cee2c365a7ea7d2
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-04-30 23:05:57 -0500

    Update getOnebyIdStudent.useCase.ts

---
+++ BACK-LEARNING-00-StudientList/src/estudiante/app/caseUse/getOnebyIdStudent.useCase.ts
@@ -0,0 +1,16 @@
+import { ClassEstudiante } from "../../domain/interface/model/estudiante.model";
+import { RepositoryEstudiante } from "../../domain/interface/repository/repositoryStudent.repository.interface";
+import { IdStudent } from "../../domain/validation/model/idStudent.value";
+import { StudentNotFound } from "../../domain/validation/util/studentNotFound.util";
+
+export class CaseUseGetOneByIdStudent{
+    constructor(private repo:RepositoryEstudiante){}
+
+    async run(id:number):Promise<ClassEstudiante>{
+        const estudiante=await this.repo.getOneById(new IdStudent(id))
+
+        if(!estudiante)throw new StudentNotFound(`Estudiante no se pude encontrar!`)
+
+        return estudiante
+    }
+}

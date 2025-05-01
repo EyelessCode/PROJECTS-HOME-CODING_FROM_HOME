491035c39ab7b4ecd4e2e55467b9aeb2ad9b698d
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-04-30 20:17:05 -0500

    Update apellidoStudent.value.ts

---
+++ BACK-LEARNING-00-StudientList/src/estudiante/domain/validation/model/apellidoStudent.value.ts
@@ -0,0 +1,22 @@
+export class ApellidoStudent{
+    apellido:string
+
+    constructor(apellido:string) {
+        this.apellido=apellido
+        this.isEmpty()
+        this.limitCharacter()
+    }
+
+    private isEmpty(){
+        if (!this.apellido||this.apellido.trim()==='') {
+            throw new Error(`El apellido no puede estar vacío!`)
+        }
+    }
+
+    private limitCharacter(){
+        if (this.apellido.length>=25||this.apellido.length<=3) {
+            throw new Error(`La cantidad de caracteres sobrepasaron `+
+                `un límite. No lo excedas!`)
+        }
+    }
+}

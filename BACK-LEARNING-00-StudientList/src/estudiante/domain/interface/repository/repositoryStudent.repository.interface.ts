0f1c000980cae828eac87ad11c47465dfc5d0892
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-05-02 16:27:49 -0500

    Cambio de nombre en la clase.

--- BACK-LEARNING-00-StudientList/src/estudiante/domain/interface/repository/repositoryStudent.repository.interface.ts
+++ BACK-LEARNING-00-StudientList/src/estudiante/domain/interface/repository/repositoryStudent.repository.interface.ts
@@ -1,7 +1,7 @@
 import { IdStudent } from '../../validation/model/idStudent.value';
 import { ClassEstudiante } from '../model/estudiante.model';
 
-export interface RepositoryEstudiante{
+export interface IRepositoryEstudiante{
     getAll():Promise<ClassEstudiante[]>
     getOneById(id:IdStudent):Promise<ClassEstudiante|null>
     create(student:ClassEstudiante):Promise<void>

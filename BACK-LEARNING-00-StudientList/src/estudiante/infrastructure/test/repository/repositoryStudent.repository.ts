a320303e237ccd49c6b5dbc86fd3ffa89293f8fc
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-05-02 16:33:44 -0500

    Cambio de lugar

---
+++ BACK-LEARNING-00-StudientList/src/estudiante/infrastructure/test/repository/repositoryStudent.repository.ts
@@ -0,0 +1,27 @@
+import { ClassEstudiante } from "../../../domain/interface/model/estudiante.model";
+import { IRepositoryEstudiante } from "../../../domain/interface/repository/repositoryStudent.repository.interface";
+import { IdStudent } from "../../../domain/validation/model/idStudent.value";
+
+export class RepositoryStudent implements IRepositoryEstudiante{
+    private estudiante:ClassEstudiante[]=[]
+
+    async getAll(): Promise<ClassEstudiante[]> {
+        return this.estudiante
+    }
+
+    async getOneById(id: IdStudent): Promise<ClassEstudiante | null> {
+        return this.estudiante.find((student)=>student.id.id===id.id)||null
+    }
+
+    async create(student: ClassEstudiante): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+
+    async edit(student: ClassEstudiante): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+
+    async delete(id: IdStudent): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+}

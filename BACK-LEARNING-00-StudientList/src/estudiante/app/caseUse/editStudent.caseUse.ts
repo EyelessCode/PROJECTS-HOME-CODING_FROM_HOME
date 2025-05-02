0a9d32672b79d4f42f9f6418e59004365619c8b6
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-05-02 00:09:40 -0500

    Update editStudent.caseUse.ts

---
+++ BACK-LEARNING-00-StudientList/src/estudiante/app/caseUse/editStudent.caseUse.ts
@@ -0,0 +1,27 @@
+import { ClassEstudiante } from "../../domain/interface/model/estudiante.model";
+import { RepositoryEstudiante } from "../../domain/interface/repository/repositoryStudent.repository.interface";
+import { ApellidoStudent } from "../../domain/validation/model/apellidoStudent.value";
+import { EdadStudent } from "../../domain/validation/model/EdadStudent.value";
+import { IdStudent } from "../../domain/validation/model/idStudent.value";
+import { NombreStudent } from "../../domain/validation/model/nombreStudent.value";
+import { StudentNotFound } from "../../domain/validation/util/studentNotFound.util";
+
+export class CaseUseEditStudent{
+    constructor(private repo:RepositoryEstudiante){}
+
+    async run(id:number,nombre:string,
+        apellido:string,edad:number
+    ):Promise<void>{
+        const estudiante=new ClassEstudiante(
+            new IdStudent(id),
+            new NombreStudent(nombre),
+            new ApellidoStudent(apellido),
+            new EdadStudent(edad)
+        )
+
+        const estudianteExiste=await this.repo.getOneById(estudiante.id)
+        if(!estudianteExiste)throw new StudentNotFound(`No se encontró un estudiante!`)
+
+        return this.repo.edit(estudiante)
+    }
+}

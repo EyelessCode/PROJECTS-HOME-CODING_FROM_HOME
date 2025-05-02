f1514a834c9ab2da807be5be2f170e52b2b45455
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-05-02 00:14:06 -0500

    Update deleteStudent.caseUse.ts

---
+++ BACK-LEARNING-00-StudientList/src/estudiante/app/caseUse/deleteStudent.caseUse.ts
@@ -0,0 +1,16 @@
+import { RepositoryEstudiante } from "../../domain/interface/repository/repositoryStudent.repository.interface";
+import { IdStudent } from "../../domain/validation/model/idStudent.value";
+import { StudentNotFound } from "../../domain/validation/util/studentNotFound.util";
+
+export class CaseUseDeleteStudent{
+    constructor(private repo:RepositoryEstudiante){}
+
+    async run(id:number):Promise<void>{
+        const estudiante=new IdStudent(id)
+
+        const estudianteExiste=await this.repo.getOneById(estudiante)
+        if(!estudianteExiste)throw new StudentNotFound(`No se encontró el estudiante!`)
+
+        await this.repo.delete(estudiante)
+    }
+}

6b270e9aa82535708265452939a40b7eae13d1bb
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-05-04 00:09:42 -0500

    Update idStudent.value.ts

--- BACK-LEARNING-00-StudientList/src/estudiante/domain/validation/model/idStudent.value.ts
+++ BACK-LEARNING-00-StudientList/src/estudiante/domain/validation/model/idStudent.value.ts
@@ -4,11 +4,24 @@ export class IdStudent{
     constructor(id:number) {
         this.id=id
         this.numNegativo()
+        this.noFloat()
     }
 
     private numNegativo(){
         if (this.id<0) {
             throw new Error(`El ID no puede ser menor a '0'!`)
+        }
+    }
+
+    private noFloat(){
+        if (!Number.isInteger(this.id)) {
+            throw new Error(`El ID debe de ser un número entero!`)
+        }
+    }
+
+    private noRepeat(){
+        if (this.id===this.id) {
+            throw new Error(`${this.id} ya existr!`)
         }
     }
 }

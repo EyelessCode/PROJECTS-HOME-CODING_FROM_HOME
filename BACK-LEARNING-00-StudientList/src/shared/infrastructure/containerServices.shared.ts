c187b81b16b8f4558404279b4b3259f184d51d79
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-05-03 22:44:29 -0500

    Update containerServices.shared.ts

--- BACK-LEARNING-00-StudientList/src/shared/infrastructure/containerServices.shared.ts
+++ BACK-LEARNING-00-StudientList/src/shared/infrastructure/containerServices.shared.ts
@@ -3,9 +3,10 @@ import { CaseUseDeleteStudent } from "../../estudiante/app/caseUse/deleteStudent
 import { CaseUseEditStudent } from "../../estudiante/app/caseUse/editStudent.caseUse";
 import { CaseUseGetAllStudent } from "../../estudiante/app/caseUse/getAllStudent.caseUse";
 import { CaseUseGetOneByIdStudent } from "../../estudiante/app/caseUse/getOnebyIdStudent.caseUse";
+import { RepositoryPostgresStudent } from "../../estudiante/infrastructure/repository/repositoryStudent.postgres.repository";
 import { RepositoryInMemoryStudent } from "../../estudiante/infrastructure/test/repository/repositoryInMemoryStudent.repository";
 
-const repo=new RepositoryInMemoryStudent()
+const repo=new RepositoryPostgresStudent(`postgresql://eyelesscode:cris03022@localhost:5432/student_temp`)
 
 const container={
     student:{

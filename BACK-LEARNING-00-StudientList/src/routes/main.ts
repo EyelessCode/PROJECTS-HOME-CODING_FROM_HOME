ddc8be325722cafe1f8b81e9eb4654bc41296eb5
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-05-03 21:05:19 -0500

    Update main.ts

--- BACK-LEARNING-00-StudientList/src/routes/main.ts
+++ BACK-LEARNING-00-StudientList/src/routes/main.ts
@@ -1,9 +1,17 @@
-import { Request, Response, Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
+import { ControllerStudent } from "../estudiante/infrastructure/controller/controllerStudent.controller";
 
 const ruta=Router()
+const controlador=new ControllerStudent()
 
-ruta.get("/html",(req:Request,res:Response)=>{
+/* ruta.get("/testing",(req:Request,res:Response)=>{
     res.send(`!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!`)
-})
+}) */
+
+ruta.get("/html",controlador.getAll)
+ruta.get("/html/:id",controlador.getOneById)
+ruta.post("/html",controlador.create)
+ruta.put("/html/:id",controlador.edit)
+ruta.delete("/html/:id",controlador.delete)
 
 export {ruta}

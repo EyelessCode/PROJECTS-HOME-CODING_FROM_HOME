4c36f57e8f6cbf754827e95d28ed02ac6cb0f361
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-05-03 21:04:31 -0500

    Update app.ts

--- BACK-LEARNING-00-StudientList/src/app.ts
+++ BACK-LEARNING-00-StudientList/src/app.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import cors from 'cors';
 import 'dotenv/config'
 import { ruta } from "./routes";
@@ -9,6 +9,21 @@ const PORT=process.env.PORT||4000
 app.use(cors())
 app.use(express.json())
 
-app.use("/test",ruta)
+// app.use("/test",ruta)
+app.use(ruta)
+
+
+app.use((err:unknown,req:Request,res:Response,next:NextFunction):any=>{
+    if(err instanceof Error){
+        console.error(err.stack);
+        return res.status(500).json({
+            message:err.message
+        })
+    }
+
+    return res.status(500).json({
+        message:`Algo salió mal!`
+    })
+})
 
 app.listen(PORT,()=>console.log(`=> http://localhost:${PORT}`))

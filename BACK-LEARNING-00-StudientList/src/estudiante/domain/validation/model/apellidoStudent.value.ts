6db3d15b38edb89fb71585f8c009ef1bda2a9c96
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-05-07 19:52:51 -0500

    Update apellidoStudent.value.ts
    Acá es la clase de validación, aplicamos el campo opcional.
    En el método camibó un poco pero es igaual.

--- BACK-LEARNING-00-StudientList/src/estudiante/domain/validation/model/apellidoStudent.value.ts
+++ BACK-LEARNING-00-StudientList/src/estudiante/domain/validation/model/apellidoStudent.value.ts
@@ -1,20 +1,20 @@
 export class ApellidoStudent{
-    apellido:string
+    apellido?:string
 
     constructor(apellido:string) {
         this.apellido=apellido
-        this.isEmpty()
+        // this.isEmpty()
         this.limitCharacter()
     }
 
-    private isEmpty(){
+    /* private isEmpty(){
         if (!this.apellido||this.apellido.trim()==='') {
             throw new Error(`El apellido no puede estar vacío!`)
         }
-    }
+    } */
 
     private limitCharacter(){
-        if (this.apellido.length>=25||this.apellido.length<=3) {
+        if (this.apellido && (this.apellido.length >= 25 || this.apellido.length <= 3)) {
             throw new Error(`La cantidad de caracteres sobrepasaron `+
                 `un límite. No lo excedas!`)
         }

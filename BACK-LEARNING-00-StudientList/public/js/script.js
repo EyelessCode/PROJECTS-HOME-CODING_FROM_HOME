71fe9ee622df57ffd6ac31a007435f3b7a687d8d
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-06-16 10:01:19 -0500

    Update script.js

--- BACK-LEARNING-00-StudientList/public/js/script.js
+++ BACK-LEARNING-00-StudientList/public/js/script.js
@@ -1,9 +1,18 @@
 import { cargaEstudiantes, crearEstudiante } from "./load/load.js"
 import { tablaEstudiantes } from "./util/util.js"
 
+
 document.addEventListener("DOMContentLoaded",async ()=>{
     const estudiante=await cargaEstudiantes()
     tablaEstudiantes(estudiante)
+})
+
+document.querySelector("#btnAbrirMenuMobile").addEventListener("click",()=>{
+    document.querySelector(".nav_container").classList.add("nav_visible")
+})
+
+document.querySelector("#btnCerrarMenuMobile").addEventListener("click",()=>{
+    document.querySelector(".nav_container").classList.remove("nav_visible")
 })
 
 document.querySelector(".form_content").addEventListener("submit",async (e)=>{

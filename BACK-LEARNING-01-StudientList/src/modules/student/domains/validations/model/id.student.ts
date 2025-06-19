b0c79b2ed95b021f1ecfee41e054aeea73336881
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-06-19 16:47:12 -0500

    Update id.student.ts

--- BACK-LEARNING-01-StudientList/src/modules/student/domains/validations/model/id.student.ts
+++ BACK-LEARNING-01-StudientList/src/modules/student/domains/validations/model/id.student.ts
@@ -1,13 +1,16 @@
+import { IdNegative } from "../util/ErrorStudent.util"
+
 export class IdStudent{
     value:number
 
     constructor(value:number) {
         this.value=value
+        this.validationNumber()
     }
 
     private validationNumber() {
         if (this.value<=0) {
-            throw new 
+            throw new IdNegative(`That's a negative number!`)
         }
     }
 }

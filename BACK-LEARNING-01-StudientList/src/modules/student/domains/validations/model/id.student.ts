1a4ac2de662f2940f7641ca625fe0a6672ba8e5a
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-06-19 16:45:21 -0500

    Changing to place again.

---
+++ BACK-LEARNING-01-StudientList/src/modules/student/domains/validations/model/id.student.ts
@@ -0,0 +1,13 @@
+export class IdStudent{
+    value:number
+
+    constructor(value:number) {
+        this.value=value
+    }
+
+    private validationNumber() {
+        if (this.value<=0) {
+            throw new 
+        }
+    }
+}

174119105bab9efa70a38a0ddd40d6401f34ccb4
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-06-19 19:10:38 -0500

    Update ic.student.ts

---
+++ BACK-LEARNING-01-StudientList/src/modules/student/domains/validations/model/ic.student.ts
@@ -0,0 +1,21 @@
+import { IcCharacterLimit, IcNoSpace } from "../util/ErrorStudent.util"
+
+export class IcStudent{
+    value:string
+
+    constructor(value:string){
+        this.value=value
+        this.characterLimit()
+    }
+
+    private characterLimit(){
+        let noSpace=this.value.trim();
+        if (noSpace.length>10) {
+            throw new IcCharacterLimit(`This student's IC pass the 10 character limit!`)
+        }
+
+        if (!this.value||noSpace.trim()===''||noSpace.length<=0) {
+            throw new IcNoSpace(`Student's IC cannot be empty!`)
+        }
+    }
+}

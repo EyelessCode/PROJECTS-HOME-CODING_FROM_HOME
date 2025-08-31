3eee309f8e28da4ce5ef449cb13316e18652d4be
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-08-31 01:08:15 -0500

    Update InfoCharacter.tsx

--- REACTJS-LEARNING-01-LocalData_SimplePage/src/components/molecule/InfoCharacter.tsx
+++ REACTJS-LEARNING-01-LocalData_SimplePage/src/components/molecule/InfoCharacter.tsx
@@ -12,13 +12,13 @@ type MiniInfoCharacterProps={
     unknownFactionUrl:string
     factionName:string,
     //? For quick view characteristics | icon
-    iconSexUrl:string,
+    // iconSexUrl:string,
     iconSpecialtyUrl:string,
     iconAttributeUrl:string,
     //? For quick view characteristics | letter
-    specialty?:string
+    specialty?:string|null
     attribute:string
-    sex?:string
+    sex?:string|null
 }
 
 type InfoCharacter={
@@ -33,7 +33,7 @@ const InfoCharacter:React.FC<InfoCharacter> = ({characterMini,title}) => {
                 {title}
             </h1>
             {characterMini.map((ch,i)=>{
-                let gender=null
+                let gender="Undefined gender"
                 if (!ch.sex) {
                     ch.sex="Undefined gender"
                 }else{
@@ -46,7 +46,7 @@ const InfoCharacter:React.FC<InfoCharacter> = ({characterMini,title}) => {
                     }
                 }
                 return(
-                    <section key={i}>
+                    <section key={i} title={ch.name}>
                         <article className="relative p-2" style={{backgroundColor:`${ch.bgColor}`}}>
                             <div className="w-45 overflow-hidden">
                                 <img loading="lazy" className="w-full h-full object-cover" src={ch.iconCharacterUrl} alt={ch.name} />
@@ -54,23 +54,23 @@ const InfoCharacter:React.FC<InfoCharacter> = ({characterMini,title}) => {
                             <div className="absolute top-12 outline rounded-full">
                                 <img loading="lazy" className="w-12" src={ch.iconFactionUrl?ch.iconFactionUrl:ch.unknownFactionUrl} alt={ch.factionName} />
                             </div>
-                            <div className="flex flex-col text-center">
+                            <div className="flex flex-col text-center px-3">
                                 <h3 className="text-4xl">{ch.name}</h3>
                                 <h4 className="text-2xl">{ch.factionName?ch.factionName:"Unkown Faction"}</h4>
                             </div>
-                            <div className="flex flex-col">
-                                <article className="flex flex-row gap-3">
-                                    <img loading="lazy" className="w-8" src={ch.iconSpecialtyUrl} alt={ch.specialty} />
+                            <div className="flex flex-col text-xl mx-22 gap-1 text-white font-semibold">
+                                <article className="flex flex-row gap-2 items-center bg-black/50 px-2 py-1 rounded-full">
+                                    <img loading="lazy" className="w-8" src={ch.iconSpecialtyUrl} alt={ch.specialty?ch.specialty:undefined} />
                                     <span>{ch.specialty}</span>
                                 </article>
-                                <article className="flex flex-row gap-3">
+                                <article className="flex flex-row gap-2  items-center bg-black/50 px-2 py-1 rounded-full">
                                     <img loading="lazy" className="w-8" src={ch.iconAttributeUrl} alt={ch.attribute} />
                                     <span>{ch.attribute}</span>
                                 </article>
-                                <article className="flex flex-row gap-3">
+                                <article className="flex flex-row gap-2  items-center bg-black/50 px-2 py-1 rounded-full">
                                     {/* This fucking shit gave me a fucking hard night! */}
                                     {/* {ch.sex?.includes("Male")?(<Icon icon="mdi:gender-male" width="28" height="28" />):(<Icon icon="mingcute:female-line" width="28" height="28" />)} */}
-                                    {ch.sex?(<Icon icon={`icons8:${gender}`} width="28" height="28" />):""}
+                                    {ch.sex?(<Icon icon={`icons8:${gender}`} width="32" height="32" />):undefined}
                                     <span>{ch.sex}</span>
                                 </article>
                             </div>

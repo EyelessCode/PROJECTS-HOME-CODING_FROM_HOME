1634191ffdef0c8404080636f89921004dd83c6d
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-11-09 19:51:20 -0500

    Add BookLoan and BookLoanId models; refactor App
    Introduces BookLoan and BookLoanId classes for the loans domain, including value object logic and validation. Refactors App.java by moving it from shared/ to the root src directory and updates its package declaration.

---
+++ Java-LEARNING-LibraryHexagonalMedium/src/modules/loans/domain/models/BookLoan.java
@@ -0,0 +1,5 @@
+package modules.loans.domain.models;
+
+public class BookLoan {
+    
+}
---
+++ Java-LEARNING-LibraryHexagonalMedium/src/modules/loans/domain/models/valueObjects/BookLoanId.java
@@ -0,0 +1,54 @@
+package modules.loans.domain.models.valueObjects;
+
+import shared.exceptions.GenericNumberInvalidException;
+import shared.exceptions.validators.GenericNumericValidator;
+
+public class BookLoanId extends GenericNumericValidator{
+    private static Byte currentId;
+    private final Byte value;
+
+    public BookLoanId(Byte value){
+        if (!byteNotNull(value)) {
+            this.value=generatedId();
+        }else{
+            onlyPositiveByte(value);
+            this.value=value;
+        }
+    }
+
+    public Byte getValue() {
+        return value;
+    }
+
+    private synchronized Byte generatedId(){
+        if (currentId==Byte.MAX_VALUE) {
+            throw new GenericNumberInvalidException("Loan IDs has been reached.");
+        }
+        return currentId++;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((value == null) ? 0 : value.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        BookLoanId other = (BookLoanId) obj;
+        if (value == null) {
+            if (other.value != null)
+                return false;
+        } else if (!value.equals(other.value))
+            return false;
+        return true;
+    }
+}

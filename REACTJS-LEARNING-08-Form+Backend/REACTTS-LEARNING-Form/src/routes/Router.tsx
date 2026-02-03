049a60f2852348b443509e74e419099fbdd5b847
EyelessCode <cristhianjosue03@outlook.com>
2026-02-03 14:28:08 -0500

    Corrección en Router, y integración de TailwindCSS.
    Se agregó TailwindCSS para un mejor diseño rápido en diferentes partes. Por lo tanto se borró App.css, se integró en vite, y se editó index.css.

--- REACTJS-LEARNING-08-Form+Backend/REACTTS-LEARNING-Form/src/routes/Router.tsx
+++ REACTJS-LEARNING-08-Form+Backend/REACTTS-LEARNING-Form/src/routes/Router.tsx
@@ -1,19 +1,21 @@
-import { Route, Routes } from "react-router-dom"
-import NotFound from "../pages/NotFound"
+import { Navigate, Route, Routes } from "react-router-dom"
+// import NotFound from "../pages/NotFound"
 import Register from "../pages/Register"
 import Home from "../pages/HomeIn"
 import Login from "../pages/Login"
 import HomeOut from "../pages/HomeOut"
+import About from "../pages/About"
 
 
 const MyRouter = () => {
     return (
         <Routes>
+            <Route element={<Home/>} path="/myTy/home" index/>
             <Route element={<HomeOut/>} path="/myTy" index/>
-            <Route element={<Login/>} path="/login"/>
-            <Route element={<Home/>} path="/home" index/>
-            <Route element={<Register/>} path="/register"/>
-            <Route element={<NotFound/>} path="*"/>
+            <Route element={<Login/>} path="/myTy/login"/>
+            <Route element={<About/>} path="/myTy/about"/>
+            <Route element={<Register/>} path="/myTy/register"/>
+            <Route element={<Navigate to="/myTy"/>} path="*"/>
         </Routes>
     )
 }
